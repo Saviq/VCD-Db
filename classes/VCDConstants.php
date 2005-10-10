13081a3c83c07737807169b210ecbf378dbbdda8
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2005-10-10 01:00:59 +0000

    Added Constants for using with LDAP Authentication.

--- classes/VCDConstants.php
+++ classes/VCDConstants.php
@@ -15,25 +15,24 @@
 ?>
 <? 
 // All constants used by VCD-db will be placed here
-define("VCDDB_VERSION","0.973");					// VCD-db version
-define("STYLE","includes/templates/default/");		// Path to current template
-define("TEMP_FOLDER","upload/");					// Temp folder used by VCD-db
-define("CACHE_FOLDER","upload/cache/");				// Fetch cache folder
-define("THUMBNAIL_PATH","upload/thumbnails/");		// Thumbnail path
-define("COVER_PATH","upload/covers/");				// Covers path
-define("PORNSTARIMAGE_PATH","upload/pornstars/");	// Pornstar images
+define("VCDDB_VERSION","0.974");						// VCD-db version
+define("STYLE","includes/templates/default/");			// Path to current template
+define("TEMP_FOLDER","upload/");						// Temp folder used by VCD-db
+define("CACHE_FOLDER","upload/cache/");					// Fetch cache folder
+define("THUMBNAIL_PATH","upload/thumbnails/");			// Thumbnail path
+define("COVER_PATH","upload/covers/");					// Covers path
+define("PORNSTARIMAGE_PATH","upload/pornstars/");		// Pornstar images
 
 // Proxy settings | if using proxy server, define it here below
-define("USE_PROXY",  "SETUP_PROXY");				// Change to "1" if using proxy server
-define("PROXY_URL",  "SETUP_PROXYNAME");			// Url of your proxy server
-define("PROXY_PORT", "SETUP_PROXYPORT");			// Proxy port
-
+define("USE_PROXY",  0);								// Change to "1" if using proxy server
+define("PROXY_URL",  "");								// Url of your proxy server
+define("PROXY_PORT", 8080);								// Proxy port
 
 // RSS Settings
-define("RSS_CACHE_TIME",7200);						// 2 hours
+define("RSS_CACHE_TIME",7200);							// 2 hours
 
 // IMDB fetch settings
-define("IMDB_MAXRESULT",50);						// Maximum count of results to display from the imdb search
+define("IMDB_MAXRESULT",50);							// Maximum count of results to display from the imdb search
 
 // Database settings
 define("DB_TYPE",	"SETUP_TYPE");
@@ -41,5 +40,12 @@ define("DB_USER",	"SETUP_USER");
 define("DB_PASS",	"SETUP_PASSWORD");
 define("DB_HOST",	"SETUP_HOST");
 define("DB_CATALOG","SETUP_CATALOG");
+
+// Authentication Method
+define("LDAP_AUTH", 0);									// Are you using LDAP for authentication ?
+define("LDAP_HOST", "");								// LDAP host name (and port if not using default)
+define("LDAP_BASEDN", "");								// LDAP Base DN for Binding to LDAP server
+define("LDAP_AD", 0);									// Is this LDAP server an Active Directory Server ?
+define("AD_DOMAIN", "");								// If server is an AD server, domain name must be specified.
 
 ?>
