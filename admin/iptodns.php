1f8a8b33442d003624ba7ffaeb5d4b8fc2082fc1
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2005-11-01 23:30:53 +0000

    Show DNS from IP on log viewer

---
+++ admin/iptodns.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<title>Resolver</title>
+	<style>
+    body {
+ 	background: #F6F6F6;
+	font: 11px/1.2 Verdana, Arial, Helvetica, sans-serif; /*background:#ddd;*/
+	margin: 1px;
+	padding: 1px;
+}
+    </style>
+
+</head>
+<body leftmargin="0" topmargin="0">
+<?
+echo gethostbyaddr($_GET['ip']);
+?>
+</body>
+</html>
