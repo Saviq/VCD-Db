c89ab0c5d5799fcca9f80bde9ee25fc2a3b7cadb
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2005-11-18 22:11:36 +0000

    The search page was not using the selected user's template from cookie if he had selected some other than the default template.

--- search.php
+++ search.php
@@ -31,8 +31,11 @@ if (isset($_GET['searchstring']) && isset($_GET['by'])) {
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<title>VCD Gallery</title>
-	<link rel="stylesheet" type="text/css" href="<?=STYLE?>/style.css"/>
+	<title>VCD-db</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=<?= VCDUtils::getCharSet()?>"/>
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" /> 
+	<link rel="stylesheet" type="text/css" href="<?= VCDUtils::getStyle()?>" media="screen, projection"/>
+	<?= VCDUtils::getAlternateLink() ?>
 	<script src="includes/js/main.js" type="text/javascript"></script>
 </head>
 <body>
