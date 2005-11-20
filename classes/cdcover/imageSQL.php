021bcf9747d25981fbecae29d98b5f5d2e33b3b2
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2005-11-20 01:17:20 +0000

    Added function for the vcd_image.php cleanup. Also added TRY - Catch blocks where needed.

--- classes/cdcover/imageSQL.php
+++ classes/cdcover/imageSQL.php
@@ -39,6 +39,10 @@
 	 			
 
 		
+		/**
+		 * Object constructor.
+		 *
+		 */
 		public function __construct() {
 			$conn = VCDClassFactory::getInstance('Connection');
 	 		$this->db = &$conn->getConnection();
@@ -57,48 +61,55 @@
 		*/
 		public function addImage($name = 'Unnamed image', $file_type = 'image/jpg', $file_size = 0, $stream = null) {
 			
-			if ($file_size == 0 || $stream == null) {
-				return false;
-			}
+			try {
 			
 			
-			// Figure out the name of the file and extension ..
-			
-			
-			$query = "INSERT INTO $this->TABLE_images (name, image_type, image_size, image)
-					  VALUES (".$this->db->qstr($name).",".$this->db->qstr($file_type).", $file_size, null)";
-
-			// Execute the query
-			$this->db->Execute($query);
-			
-			// Get the last inserted ID
-			$new_imageid = $this->db->Insert_Id();
-			
-			if ($this->conn->getSQLType() == 'postgres7') {
-				$new_imageid =  $this->conn->oToID($this->TABLE_images, 'image_id');
-			}
-			
-			
-			// is the new imageID ok ?
-			if (!is_numeric($new_imageid)) {
-				// seems not ok .. let's get last inserted id manually
-				$query = "SELECT image_id FROM $this->TABLE_images ORDER BY image_id DESC";
-				$rs = $this->db->SelectLimit($query, 1);
-				// Should only be 1 recordset
-				foreach ($rs as $row) {
-					$new_imageid = $row[0];
+				if ($file_size == 0 || $stream == null) {
+					return false;
 				}
-				$rs->Close();
-			}
-			
-			
-			$update_value = "image_id=" . $new_imageid;
-			
-			
-			// Update the BLOB field with the actual MIME64 stream
-		   	$this->db->UpdateBlob($this->TABLE_images,'image', $stream, $update_value);	
+				
+				
+				// Figure out the name of the file and extension ..
+				
+				
+				$query = "INSERT INTO $this->TABLE_images (name, image_type, image_size, image)
+						  VALUES (".$this->db->qstr($name).",".$this->db->qstr($file_type).", $file_size, null)";
+	
+				// Execute the query
+				$this->db->Execute($query);
+				
+				// Get the last inserted ID
+				$new_imageid = $this->db->Insert_Id();
+				
+				if ($this->conn->getSQLType() == 'postgres7') {
+					$new_imageid =  $this->conn->oToID($this->TABLE_images, 'image_id');
+				}
+				
+				
+				// is the new imageID ok ?
+				if (!is_numeric($new_imageid)) {
+					// seems not ok .. let's get last inserted id manually
+					$query = "SELECT image_id FROM $this->TABLE_images ORDER BY image_id DESC";
+					$rs = $this->db->SelectLimit($query, 1);
+					// Should only be 1 recordset
+					foreach ($rs as $row) {
+						$new_imageid = $row[0];
+					}
+					$rs->Close();
+				}
+				
+				
+				$update_value = "image_id=" . $new_imageid;
+				
+				
+				// Update the BLOB field with the actual MIME64 stream
+			   	$this->db->UpdateBlob($this->TABLE_images,'image', $stream, $update_value);	
+			   	
+			   	return $new_imageid;
 		   	
-		   	return $new_imageid;
+			} catch (Exception $ex) {
+				throw new Exception($ex->getMessage());
+			}
 			
 		}
 		
@@ -110,15 +121,51 @@
 		 * @return string
 		 */
 		public function getImageStream($image_id) {
-		
-			$query = "SELECT image FROM $this->TABLE_images WHERE image_id =". $image_id;
-			$stream = $this->db->GetOne($query);
-			if (!$stream) {
-				return false;
-			} else {
-				return base64_decode($stream);	
+			
+			try {
+			
+				$query = "SELECT image FROM $this->TABLE_images WHERE image_id =". $image_id;
+				$stream = $this->db->GetOne($query);
+				if (!$stream) {
+					return false;
+				} else {
+					return base64_decode($stream);	
+				}
+			
+			} catch (Exception $ex) {
+				throw new Exception($ex->getMessage());
 			}
 			
+		}
+		
+		/**
+		 * Get all image data from database.  Except for the binary image stream.
+		 *
+		 * @param int $image_id
+		 * @return array
+		 */
+		public function getImageDetails($image_id) {
+			try {
+			
+				if (!is_numeric($image_id)) {
+					throw new Exception("Invalid image ID");
+				}
+				
+				
+				$query = "SELECT image_id, name, image_type, image_size FROM $this->TABLE_images
+						  WHERE image_id = " . $image_id;
+				$rs = $this->db->Execute($query);
+				if ($rs) {
+					return $rs->GetRowAssoc();
+				} else {
+					return null;
+				}
+				
+				
+			} catch (Exception $ex) {
+				throw new Exception($ex->getMessage());
+			} 
+		
 		}
 		
 		
