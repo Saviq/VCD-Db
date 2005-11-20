e899852756466c99b87f8165fa0188e18db72527
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2005-11-20 01:13:16 +0000

    Fixed how the image information gets rendered. The image name and size are now available and IE does not try to save the image as Bitmap

--- vcd_image.php
+++ vcd_image.php
@@ -1,9 +1,16 @@
 <?php
 require_once("classes/includes.php");
-$IMGclass = VCDClassFactory::getInstance("VCDImage");
 $image_id = $_GET['id'];
-Header ('Content-type: image/pjpeg');
-Header ('Content-Disposition: inline; filename=mynd.jpg');  
-print $IMGclass->getImageStream($image_id);
+$CLASSImage = new VCDImage($image_id);
+
+@session_write_close();
+@ob_end_clean();
+header("Cache-Control: ");
+header("Pragma: ");
+header("Content-Type: application/octet-stream");
+header("Content-Length: " .(string)($CLASSImage->getFilesize()) );
+header('Content-Disposition: attachment; filename="'.$CLASSImage->getImageName().'"');
+header("Content-Transfer-Encoding: binary\n");
+echo $CLASSImage->getImageStream($image_id);
 ?>
 
