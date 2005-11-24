604bdf6aa43daff6b15b6d5dec1b437ad08bc54b
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2005-11-24 01:58:45 +0000

    TopTen list changed behavior.

--- modules/sidebar_right.php
+++ modules/sidebar_right.php
@@ -4,11 +4,13 @@
 	$SETTINGSClass = VCDClassFactory::getInstance("vcd_settings");
 	
 	$maxTitlelen = 17;
-
+	$tv_category = $SETTINGSClass->getCategoryIDByName("tv shows");
+	$xx_category = $SETTINGSClass->getCategoryIDByName("adult");
 	
 	
-	/* Top Ten latest all movies */
-	$movies = $VCDClass->getTopTenList();
+	/* Top Ten latest all movies , except for TV Shows and Adult movies*/
+	$arrExclude = array($tv_category, $xx_category);
+	$movies = $VCDClass->getTopTenList(0, $arrExclude);
 	if (sizeof($movies) > 0) {
 		print "<table cellspacing=\"0\" cellpadding=\"0\" border=\"0\" width=\"100%\" class=\"list\">";
 		print "<tr><td class=\"header\">10 ".$language->show('X_LATESTMOVIES')."</td></tr>";
@@ -22,7 +24,6 @@
 	
 	
 	/* Top Ten latest TV shows */
-	$tv_category = $SETTINGSClass->getCategoryIDByName("tv shows");
 	if ($tv_category != 0) {
 		$movies = $VCDClass->getTopTenList($tv_category);
 		if (sizeof($movies) > 0) {
@@ -40,27 +41,32 @@
 	
 	
 	
-	/* Top Ten latest Porn flix */
-	$tv_category = $SETTINGSClass->getCategoryIDByName("adult");
-	if ($tv_category != 0) {
-		$movies = $VCDClass->getTopTenList($tv_category);
-		if (sizeof($movies) > 0) {
-			print "<table cellspacing=\"0\" cellpadding=\"0\" border=\"0\" width=\"100%\" class=\"list\">";
-			print "<tr><td class=\"header\">10 ".$language->show('X_LATESTBLUE')."</td></tr>";
-			
-			foreach ($movies as $movie) {
-				echo "<tr><td><a href=\"./?page=cd&amp;vcd_id=".$movie->getID()."\" title=\"".$movie->getTitle()."\">".  VCDUtils::shortenText($movie->getTitle(), $maxTitlelen) . "</a></td></tr>";
+	/* Top Ten latest Porn flix IF adult content is allowed and user has requested to see it .. */
+	if (VCDUtils::isLoggedIn() 
+		&& $SETTINGSClass->getSettingsByKey('SITE_ADULT') 
+		&& $_SESSION['user']->getPropertyByKey('SHOW_ADULT')) {
+	
+		if ($xx_category != 0) {
+			$movies = $VCDClass->getTopTenList($xx_category);
+			if (sizeof($movies) > 0) {
+				print "<table cellspacing=\"0\" cellpadding=\"0\" border=\"0\" width=\"100%\" class=\"list\">";
+				print "<tr><td class=\"header\">10 ".$language->show('X_LATESTBLUE')."</td></tr>";
+				
+				foreach ($movies as $movie) {
+					echo "<tr><td><a href=\"./?page=cd&amp;vcd_id=".$movie->getID()."\" title=\"".$movie->getTitle()."\">".  VCDUtils::shortenText($movie->getTitle(), $maxTitlelen) . "</a></td></tr>";
+				}
+				
+				print "</table>";
 			}
 			
-			print "</table>";
 		}
-		
+	
 	}
 	
 	// Check for custom frontpage settings
 	if (VCDUtils::isLoggedIn()) {
 		$user_id = VCDUtils::getUserID();
-		$arr = $SETTINGSClass->getMetadata(0, $user_id, 'frontstats');
+		$arr = $SETTINGSClass->getMetadata(0, $user_id, metadataTypeObj::SYS_FRONTSTATS );
 		if (is_array($arr) && sizeof($arr) == 1 && $arr[0] instanceof metadataObj) {
 			$SHOW_STATS = $arr[0]->getMetadataValue();
 			if ($SHOW_STATS) {
