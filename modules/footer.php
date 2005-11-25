5aba5d9ef141329e8890a60a449f951602827647
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2005-11-25 12:37:40 +0000

    Footer now displays number of queries executed

--- modules/footer.php
+++ modules/footer.php
@@ -1,4 +1,5 @@
 <div id="ftr" align="center">
 Page Loaded in <?=VCDUtils::getPageLoadTime()?> sec.
+(<i><?= Connection::getQueryCount()?> Queries</i>)
 &nbsp; Copyright (c) <a href="http://vcddb.konni.com" target="_new" title="Check out the official VCD-db website">VCD-db <?=VCDDB_VERSION?></a>
 </div>
