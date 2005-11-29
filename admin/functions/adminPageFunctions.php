1239af28fd2eb5ef868f6a010ab087ad88fe5645
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2005-11-29 12:35:56 +0000

    Added timelimit = 300 When delete-ing all user data.

--- admin/functions/adminPageFunctions.php
+++ admin/functions/adminPageFunctions.php
@@ -31,9 +31,9 @@ function printTr($open = true, $hover = true) {
 		$js = "onMouseOver=\"trOn(this)\" onMouseOut=\"trOff(this)\"";
 		if ($hover)
 			print "<tr $js>";
-		else 
+		else
 			print "<tr>";
-			
+
 	} else {
 		print "</tr>";
 	}
@@ -61,14 +61,14 @@ function printRowHeader($arrHeader) {
  * @param string $cssClass
  */
 function printRow($rowdata = "", $cssClass = "") {
-	
+
 	if (is_bool($rowdata)) {
-		if ($rowdata) 
+		if ($rowdata)
 			$rowdata = "True";
-		 else 
+		 else
 			$rowdata = "False";
 	}
-	
+
 	if ($cssClass != "") {
 		$cssClass = " class=".$cssClass;
 	}
@@ -107,7 +107,7 @@ function printEditRow($recordID, $recordType) {
  * @param string $jsfunction
  */
 function printCustomRow($recordID, $recordType, $image, $alt_text, $jsfunction) {
-	print "<td><img src=\"../images/admin/".$image.".gif\" border=0 hspace=3 title=\"".$alt_text."\" onClick=\"".$jsfunction."($recordID,'$jsfunction')\"></td>";	
+	print "<td><img src=\"../images/admin/".$image.".gif\" border=0 hspace=3 title=\"".$alt_text."\" onClick=\"".$jsfunction."($recordID,'$jsfunction')\"></td>";
 }
 
 /**
@@ -121,18 +121,18 @@ function printCustomRow($recordID, $recordType, $image, $alt_text, $jsfunction) 
  */
 function createDropDown($objArr, $selectName, $firstIndex = "", $cssClass = "",$selectedIndex = "") {
 	if (is_array($objArr)) {
-		
+
 		if ($cssClass != "") {
 			$cssClass = "class=\"$cssClass\"";
 		}
-		
-	
-		
+
+
+
 		print "<select name=\"".$selectName."\" size=\"1\" ".$cssClass.">";
 		if ($firstIndex != "") {
 			print "<option value=\"null\">".$firstIndex."</option>";
 		}
-		
+
 		foreach ($objArr as $obj) {
 			$data = $obj->getList();
 			if (strcmp($data['id'],$selectedIndex) == 0) {
@@ -140,10 +140,10 @@ function createDropDown($objArr, $selectName, $firstIndex = "", $cssClass = "",$
 			} else {
 				print "<option value=\"".$data['id']."\">".$data['name']."</option>";
 			}
-			
+
 		}
-		
-		print "</select>";	
+
+		print "</select>";
 	}
 }
 
@@ -167,142 +167,144 @@ function cleanItem(&$item) {
 function deleteRecord($recordID, $recordType) {
 	if (!is_numeric($recordID))
 		return;
-	
+
 	switch ($recordType) {
 		case 'settings';
 			$CLASSsettings = new vcd_settings();
 			if ($CLASSsettings->deleteSettings($recordID))
 				header("Location: ./?page=".$recordType.""); /* Redirect browser */
-			else 
+			else
 				print "<script>history.back(-1)</script>";
 		break;
-		
+
 		case 'roles';
 			$USERClass = new vcd_user();
 			if ($USERClass->deleteUserRole($recordID))
 				header("Location: ./?page=".$recordType.""); /* Redirect browser */
-			else 
+			else
 				print "<script>history.back(-1)</script>";
 		break;
-		
+
 		case 'media_types';
 			$SETTINGSclass = new vcd_settings();
 			if ($SETTINGSclass->deleteMediaType($recordID))
 				header("Location: ./?page=".$recordType.""); /* Redirect browser */
-			else 
+			else
 				print "<script>history.back(-1)</script>";
 		break;
-		
+
 		case 'categories';
 			$SETTINGSclass = new vcd_settings();
 			if ($SETTINGSclass->deleteMovieCategory($recordID))
 				header("Location: ./?page=".$recordType.""); /* Redirect browser */
-			else 
+			else
 				print "<script>history.back(-1)</script>";
 		break;
-		
-		
+
+
 		case 'cover_types';
 			$COVERSclass = new vcd_cdcover();
 			if ($COVERSclass->deleteCoverType($recordID))
 				header("Location: ./?page=".$recordType.""); /* Redirect browser */
-			else 
+			else
 				print "<script>history.back(-1)</script>";
 		break;
-		
+
 		case 'users';
 			$USERClass = new vcd_user();
 			if ($USERClass->deleteUser($recordID))
 				header("Location: ./?page=".$recordType.""); /* Redirect browser */
-			else 
+			else
 				print "<script>history.back(-1)</script>";
 		break;
-		
+
 		case 'deleteUser';
-			
+
 			// Check if user is trying to delete himself
 			if ($recordID == VCDUtils::getUserID()) {
 				VCDException::display('You cannot delete your own account!', true);
 				return;
 			}
-		
+
 			$USERClass = new vcd_user();
-						
+
 			// check if all his data should be deleted as well
 			if (isset($_GET['mode']) && strcmp($_GET['mode'], 'full') == 0) {
-				
-				
-				if ($USERClass->deleteUser($recordID, true))
-					header("Location: ./?page=".$recordType.""); 
-				else 
-					print "<script>history.back(-1)</script>";	
 
-				
+				// Add time limit since this can be very time consuming operation.
+				set_time_limit(300);
+
+				if ($USERClass->deleteUser($recordID, true))
+					header("Location: ./?page=".$recordType."");
+				else
+					print "<script>history.back(-1)</script>";
+
+
 			} else {
 				if ($USERClass->deleteUser($recordID, false))
 					header("Location: ./?page=".$recordType.""); /* Redirect browser */
-				else 
-					print "<script>history.back(-1)</script>";	
+				else
+					print "<script>history.back(-1)</script>";
 			}
-			
-			
-			
-			
+
+
+
+
 		break;
-		
+
 		case 'sites';
 			$SETTINGSclass = new vcd_settings();
 			if ($SETTINGSclass->deleteSourceSite($recordID))
 				header("Location: ./?page=".$recordType.""); /* Redirect browser */
-			else 
+			else
 				print "<script>history.back(-1)</script>";
 		break;
-		
+
 		case 'properties';
 			$USERClass = new vcd_user();
 			if ($USERClass->deleteProperty($recordID))
 				header("Location: ./?page=".$recordType.""); /* Redirect browser */
-			else 
+			else
 				print "<script>history.back(-1)</script>";
 		break;
-		
+
 		case 'xmlfeeds';
 			$SETTINGSClass = new vcd_settings();
 			if ($SETTINGSClass->delFeed($recordID))
 				header("Location: ./?page=".$recordType.""); /* Redirect browser */
-			else 
+			else
 				print "<script>history.back(-1)</script>";
 		break;
-		
-		
+
+
 		case 'log':
 			VCDLog::clearLog();
 			header("Location: ./?page=".$recordType.""); /* Redirect browser */
 			break;
-		
-		
+
+
 		case 'porncategories':
 			$PORNClass = new vcd_pornstar();
-			if ($PORNClass->deleteAdultCategory($recordID)) 
+			if ($PORNClass->deleteAdultCategory($recordID))
 				header("Location: ./?page=".$recordType.""); /* Redirect browser */
-			else 
+			else
 				print "<script>history.back(-1)</script>";
 		break;
-		
-		
+
+
 		case 'pornstudios':
 			$PORNClass = new vcd_pornstar();
 			if ($PORNClass->deleteStudio($recordID))
 				header("Location: ./?page=".$recordType.""); /* Redirect browser */
-			else 
+			else
 				print "<script>history.back(-1)</script>";
-			
+
 		break;
-		
-			
-	
+
+
+
 	}
-		
+
 }
 
 /**
@@ -346,7 +348,7 @@ function getADODBdatePart($datestamp, $format) {
 	elseif (strcmp($format, "year") == 0) {
 		$datestring =  substr($datestamp ,6 ,10);
 	}
-	
+
 	return $datestring;
 }
 
@@ -358,13 +360,13 @@ function getADODBdatePart($datestamp, $format) {
  */
 function showAddRecord($do) {
 
-	if ($do == "versioncheck" || $do == "statistics" || $do == "backup" || $do == "import" || $do == "" || 
+	if ($do == "versioncheck" || $do == "statistics" || $do == "backup" || $do == "import" || $do == "" ||
 		$do == "statistics" || $do == "roles" || $do == "log" || $do == "viewlog")  {
 		return false;
-	} 
-	
+	}
+
 	return true;
-	
+
 }
 
 /**
@@ -374,12 +376,12 @@ function showAddRecord($do) {
  */
 function checkVersion() {
 	print "<b>Checking for new version .....</b>";
-	
+
 	// Flush errors ..
 	error_reporting(0);
 	$home = "http://vcddb.konni.com/vcddbversion.xml";
 	$xml = simplexml_load_file($home);
-	
+
 	if ($xml && isset($xml->error)) {
 		print $xml->error;
 		return;
@@ -387,26 +389,26 @@ function checkVersion() {
 	if (!$xml) {
 		print "<p>Could not retrive latest version information, try again later.</p>";
 		return;
-	} 
-	
+	}
+
 	$application = $xml->application;
     $version = $xml->current_version;
-    
+
     print "<p>Application: " . $application . "<br>";
     print "Installed version => " . VCDDB_VERSION . "<br>";
     print "Latest version => " . $version . "<br><br>";
-    
+
     if (strcmp(VCDDB_VERSION, $version) == 0) {
     	print "<b>You have latest version installed.</b>";
     } else {
     	print "<b>New version available, check it out => <a href=\"http://vcddb.konni.com\" target=\"new\">http://vcddb.konni.com</a></b>";
     }
-    
+
     print "</p>";
-    
-    
-    
-    
+
+
+
+
     // Reset error reporting
 	error_reporting(ini_get('error_reporting'));
 }
@@ -417,7 +419,7 @@ function checkVersion() {
  * @param int $user_id
  */
 function exportUserXML($user_id) {
-	
+
 	$CLASSVcd = VCDClassFactory::getInstance("vcd_movie");
 	$CLASSUSer = VCDClassFactory::getInstance('vcd_user');
 	$usr = $CLASSUSer->getUserByID($user_id);
@@ -425,32 +427,32 @@ function exportUserXML($user_id) {
 		VCDException::display('Invalid user selection', true);
 		return;
 	}
-	
+
 	$arrMovies = $CLASSVcd->getAllVcdByUserId($user_id, false);
-	
-	
+
+
 	if (sizeof($arrMovies) == 0) {
 		VCDException::display('User has no movies', true);
 		return;
 	}
-	
+
 	$filename = "vcddb_export_" . $usr->getUserName() . ".xml";
-	
+
 	header("Content-type: application/xml");
 	header("Content-Disposition: attachment; filename=".$filename."");
 	$xml = "<?xml version=\"1.0\" encoding=\"iso-8859-1\" ?>";
 	$xml .= "<vcdmovies>";
-	
-	
+
+
 	foreach ($arrMovies as $vcdObj) {
 		$xml .= $vcdObj->toXML();
 	}
-	
+
 	$xml .= "</vcdmovies>";
 	unset($arrMovies);
 	print $xml;
 	exit();
-	
+
 }
 
 
