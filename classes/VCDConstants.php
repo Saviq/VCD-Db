d5eae6e0aa61c2519aeda0abc01e74e6f251da3c
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2005-11-29 01:23:24 +0000

    Standard V. 0.98 constant file. Added the VSIZE_ constants for the max filesizes for uploaded attachments and imports.

--- classes/VCDConstants.php
+++ classes/VCDConstants.php
@@ -15,8 +15,8 @@
 ?>
 <? 
 // All constants used by VCD-db will be placed here
-define("VCDDB_VERSION","0.974-cvs");					// VCD-db version
-define("STYLE","includes/templates/default/");			// Path to current template
+define("VCDDB_VERSION","0.98");							// VCD-db version
+define("STYLE","includes/templates/violet/");			// Path to current template
 define("TEMP_FOLDER","upload/");						// Temp folder used by VCD-db
 define("CACHE_FOLDER","upload/cache/");					// Fetch cache folder
 define("THUMBNAIL_PATH","upload/thumbnails/");			// Thumbnail path
@@ -48,4 +48,10 @@ define("LDAP_BASEDN", "");								// LDAP Base DN for Binding to LDAP server
 define("LDAP_AD", 0);									// Is this LDAP server an Active Directory Server ?
 define("AD_DOMAIN", "");								// If server is an AD server, domain name must be specified.
 
+// Maximum filesizes for uploading and importing images and data (in MB)
+// If entry exceeds "upload_max_filesize" in php.ini,  "upload_max_filesize" overrides.
+define("VSIZE_THUMBS", 0.2);							// Max Thumbnail filesize
+define("VSIZE_COVERS", 5);								// Max Cover filesize
+define("VSIZE_XML", 20);								// Max Imported XML filesize
+define("VSIZE_XMLTHUMBS", 30);							// Max imported XML Thumbnail filesize
 ?>
