fee7ac889c9cfcb8802aa1736a000c27dd21578b
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2005-11-30 11:07:43 +0000

    Added nowrap for the "media type"  table cell.

--- pages/category.php
+++ pages/category.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /* Display the movies in selected category */
 
 $VCDClass = VCDClassFactory::getInstance("vcd_movie");
@@ -10,18 +10,18 @@ $batch  = 0;
 if (isset($_GET['batch']))
 	$batch = $_GET['batch'];
 
-	
+
 	$imagemode = false;
-		
+
 	if (isset($_GET['viewmode']) || (isset($_SESSION['viewmode']) && strcmp($_SESSION['viewmode'], 'image') == 0)) {
 		$imagemode = true;
 		$js = "viewMode({$cat_id}, 'text', {$batch})";
-		$viewbar = "(<a href=\"#\" onclick=\"{$js}\">".$language->show('M_TEXTVIEW')."</a> / ".$language->show('M_IMAGEVIEW').")";		
+		$viewbar = "(<a href=\"#\" onclick=\"{$js}\">".$language->show('M_TEXTVIEW')."</a> / ".$language->show('M_IMAGEVIEW').")";
 	} else {
 		$js = "viewMode({$cat_id}, 'image', {$batch})";
 		$viewbar = "(".$language->show('M_TEXTVIEW')." / <a href=\"#\" onclick=\"{$js}\">".$language->show('M_IMAGEVIEW')."</a>)";
 	}
-	
+
 $showmine = false;
 $checked = "";
 if (isset($_SESSION['mine']) && $_SESSION['mine'] == true) {
@@ -33,7 +33,7 @@ if (VCDUtils::isLoggedIn()) {
 	$viewbar .= "&nbsp; | <input type=\"checkbox\" class=\"nof\" onclick=\"showonlymine(".$cat_id.")\" ".$checked."/>".$language->show('M_MINEONLY')."";
 }
 
-	
+
 $Recordcount = $SETTINGSclass->getSettingsByKey("PAGE_COUNT");
 $offset = $batch*$Recordcount;
 
@@ -52,8 +52,8 @@ if ($showmine && VCDUtils::isLoggedIn()) {
 <?
 
 if (sizeof($movies) > 0 || $showmine) {
-	
-	
+
+
 	// Display the pager
 	if ($imagemode) {
 		$suburl = "category_id=".$cat_id."&amp;viewmode=img";
@@ -61,25 +61,25 @@ if (sizeof($movies) > 0 || $showmine) {
 		$suburl = "category_id=".$cat_id;
 	}
 
-	if ($showmine && VCDUtils::isLoggedIn()) { 
+	if ($showmine && VCDUtils::isLoggedIn()) {
 		$categoryCount = $VCDClass->getCategoryCount($cat_id, false, VCDUtils::getUserID());
 	} elseif (VCDUtils::isLoggedIn() && VCDUtils::isUsingFilter(VCDUtils::getUserID())) {
 		$categoryCount = $VCDClass->getCategoryCountFiltered($cat_id, VCDUtils::getUserID());
 	} else {
 		$categoryCount = $VCDClass->getCategoryCount($cat_id);
 	}
-	
-		
+
+
 	print "&nbsp;<span class=\"bold\">".$language->show('M_CURRCAT')."</span>&nbsp;";
 	print "&nbsp; (".$categoryCount." ".$language->show('X_MOVIES').") ".$viewbar."";
-		
-	
+
+
 	pager($categoryCount, $batch, $suburl);
-	
+
 	if (!$imagemode) {
-	
+
 		print "<table cellspacing=\"0\" cellpadding=\"0\" border=\"0\" width=\"100%\" class=\"displist\">";
-		print "<tr><td class=\"header\">".$language->show('M_TITLE')."</td><td nowrap=\"nowrap\" class=\"header\">".$language->show('M_YEAR')."</td><td class=\"header\">".$language->show('M_MEDIATYPE')."</td></tr>";
+		print "<tr><td class=\"header\">".$language->show('M_TITLE')."</td><td nowrap=\"nowrap\" class=\"header\">".$language->show('M_YEAR')."</td><td class=\"header\" nowrap=\"nowrap\">".$language->show('M_MEDIATYPE')."</td></tr>";
 		foreach ($movies as $movie) {
 			print "<tr>
 					   <td width=\"70%\"><a href=\"./?page=cd&amp;vcd_id=".$movie->getID()."\">".$movie->getTitle()."</a></td>
@@ -87,25 +87,25 @@ if (sizeof($movies) > 0 || $showmine) {
 			           <td nowrap=\"nowrap\">".fixFormat($movie->showMediaTypes())."</td>
 				   </tr>";
 		}
-		print "</table>";	
-	
+		print "</table>";
+
 	} else {
 			print "<hr/>";
 			print "<div id=\"actorimages\">";
 			foreach ($movies as $movie) {
-								
+
 				$coverObj = $movie->getCover('thumbnail');
 				if ($coverObj instanceof cdcoverObj ) {
 					print $coverObj->showCategoryImageAndLink("./?page=cd&amp;vcd_id=".$movie->getID()."",$movie->getTitle());
-					
+
 				}
 			}
-			
+
 			print "</div>";
-			
+
 		}
-	
-	
+
+
 } else {
 	// Movie array has 0 entries, either because of filter or that user has no movies in this category.
 	redirect();
