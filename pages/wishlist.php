eed5fe31cfc4571fb95320c202b612663ca8ba95
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2005-12-08 01:17:52 +0000

    User can now see if he is the owner of movies on his wishlist.

--- pages/wishlist.php
+++ pages/wishlist.php
@@ -18,11 +18,16 @@
 
 <table cellspacing="0" cellpadding="0" border="0" width="100%" class="displist">
 <tr>
-	<td class="bold" width="95%"><?= $language->show('M_TITLE')?></td><td>&nbsp;</td>
+	<td class="bold" width="92%"><?= $language->show('M_TITLE')?></td><td>&nbsp;</td><td>&nbsp;</td>
 </tr>
 <? 
 	foreach ($listArr as $item) {
-		print "<tr><td><a href=\"./?page=cd&amp;vcd_id=".$item[0]."\">".$item[1]."</a></td><td align=\"center\"><a href=\"#\"><img src=\"images/icon_del.gif\" onclick=\"deleteFromWishlist(".$item[0].")\" border=\"0\"/></a></td></tr>";
+		$iown = "&nbsp;";
+		if ($item[2] == 1) {
+			$iown = "<img src=\"images/mark_seen.gif\" border=\"0\" alt=\"{$language->show('W_OWN')}\"/>";
+		}
+		
+		print "<tr><td><a href=\"./?page=cd&amp;vcd_id=".$item[0]."\">".$item[1]."</a></td><td align=\"center\">{$iown}</td><td align=\"center\"><a href=\"#\"><img src=\"images/icon_del.gif\" onclick=\"deleteFromWishlist(".$item[0].")\" border=\"0\"/></a></td></tr>";
 	}
 	
 	unset($listArr);
