f7469b064ae34814d990aff48aaba81869b6e146
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2005-12-08 19:39:10 +0000

    Users can now be added from admin section.

--- admin/forms/addUser.php
+++ admin/forms/addUser.php
@@ -1,6 +1,30 @@
 <div id="newObj" style="display: none;">
-<p style="font-weight:bold;font-size:10px;">New users must register them selves,<br/>
-On your VCD-DB front page.
-</p>
+<form name="new" method="POST">
+
+<table class="add">
+<tr>
+	<td>Full name:</td>
+	<td><input name="name" size="40" type="text"  value="" onFocus="setBorder(this)" onBlur="clearBorder(this)"></td>
+</tr>
+
+<tr>
+	<td>Username:</td>
+	<td><input name="username" size="40" type="text" value="" onFocus="setBorder(this)" onBlur="clearBorder(this)"></td></td>
+</tr>
+<tr>
+	<td>Email:</td>
+	<td><input name="email" size="40" type="text" value="" onFocus="setBorder(this)" onBlur="clearBorder(this)"></td></td>
+</tr>
+<tr>
+	<td>Password:</td>
+	<td><input name="password" size="40" type="password" value="" onFocus="setBorder(this)" onBlur="clearBorder(this)"></td></td>
+</tr>
+
+<tr>
+	<td colspan="2"><INPUT type="submit" value="Add user" name="save" class="save"></td>
+</tr>
+</table>
+				
+</form>
 <h1></h1>
 </div>
