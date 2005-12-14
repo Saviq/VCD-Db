bb1297454fa0ab34d3ed719714c4e5dae273d6b3
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2005-12-14 12:49:12 +0000

    Fixed the showImage() function. Images are now correctly displayed in the pornstar manager window.

--- classes/pornstar/pornstarObj.php
+++ classes/pornstar/pornstarObj.php
@@ -218,10 +218,10 @@ if (!defined("PORNSTARIMAGE_PATH")) {
 		 *
 		 * @param string $prefix
 		 */
-		public function showImage($prefix = '') {
+		public function showImage($prefix = "") {
 			
 			$filenotfoundImage = "notfoundimagestar.gif";
-			if (!file_exists(PORNSTARIMAGE_PATH.$this->image) && isset($this->image)) { 
+			if (!file_exists($prefix.PORNSTARIMAGE_PATH.$this->image) && isset($this->image)) { 
 				print "<a href=\"./?page=pornstar&amp;pornstar_id=".$this->id."\"><img src=\"".$prefix."images/{$filenotfoundImage}\" border=\"0\" alt=\"\" title=\"".$this->name."\" class=\"imgx\"/></a>";
 			} else if (isset($this->image) && strlen($this->image) > 3) {
 				print "<a href=\"./?page=pornstar&amp;pornstar_id=".$this->id."\"><img src=\"".$prefix.PORNSTARIMAGE_PATH . $this->image."\" class=\"imgx\" alt=\"\" title=\"".$this->name."\" width=\"145\" height=\"200\" border=\"0\"/></a>";
