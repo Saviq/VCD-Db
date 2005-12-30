0f3c76250bf6f33a6460e9f2b6f56063fc9c1fd4
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2005-12-30 00:38:19 +0000

    Updated function getMetadata() to get VERY specific metadata.

--- classes/settings/settingsSQL.php
+++ classes/settings/settingsSQL.php
@@ -968,7 +968,15 @@
 
 			$metaArr = array();
 			
-			if (!is_null($mediatype_id)) {
+			
+			if (is_numeric($mediatype_id) && is_numeric($record_id) && is_numeric($user_id) && strlen($metadata_name) == 0) {
+				$query = "SELECT m.metadata_id, m.record_id, m.user_id, n.type_name, m.metadata_value,
+						  m.mediatype_id, n.type_id, n.owner_id, n.type_description FROM $this->TABLE_metadata m
+						  LEFT OUTER JOIN $this->TABLE_metatypes n on m.type_id = n.type_id
+						  WHERE m.record_id = ".$record_id." AND m.user_id = " . $user_id . "
+						  AND m.mediatype_id = ".$mediatype_id." ORDER BY n.type_name";
+				
+			} else if (!is_null($mediatype_id)) {
 				$query = "SELECT m.metadata_id, m.record_id, m.user_id, n.type_name, m.metadata_value,
 						  m.mediatype_id, n.type_id, n.owner_id FROM $this->TABLE_metadata m
 						  LEFT OUTER JOIN $this->TABLE_metatypes n on m.type_id = n.type_id
