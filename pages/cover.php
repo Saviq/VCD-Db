8989c5db280d93549abbf2b6fbd0bf3244a63e2d
Saviq <Saviq@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2006-01-05 15:21:23 +0000

    Better image size handling. Resize images bigger than screen. Resize window to perfectly fit the image.

--- pages/cover.php
+++ pages/cover.php
@@ -11,18 +11,43 @@
 <TITLE>-&lt;::Cover::&gt;-</TITLE>
 <style type="text/css" media="all">@import url("../includes/templates/default/css/style.css");</style>
 <SCRIPT>
+var ns4 = (document.layers);
+var ie4 = (document.all && !document.getElementById);
+var ie5 = (document.all && document.getElementById);
+var ns6 = (!document.all && document.getElementById);
 function resize() {
 	try {
-		intwidth = window.document.image.width;
-		intheight = window.document.image.height;
-		window.resizeTo(intwidth,intheight) 
+    	// screen size substracted to try and fit on screen
+		var x = screen.availWidth - 5;
+		var y = screen.availHeight - 50;
+		// image bigger than screen
+		if ((window.document.image.width > x) ||
+			(window.document.image.height > y)) {
+			window.moveTo(0,0);
+			// image aspect ratio
+			var ratio = window.document.image.width / window.document.image.height;
+			// if "wider" aspect ratio
+			if (ratio > (screen.availWidth / screen.availHeight)) {
+				window.document.image.width = x;
+			// else "higher' aspect ratio
+			} else {
+				window.document.image.height = y;
+			}
+		}
+		if (ie4 || ie5) {
+			intwidth = window.document.image.width - window.document.body.clientWidth;
+			intheight = window.document.image.height - window.document.body.clientHeight;
+		} else {
+			intwidth = window.document.image.width - window.innerWidth;
+			intheight = window.document.image.height - window.innerHeight;
+		}
+		window.resizeBy(intwidth,intheight);
 	} catch (Exception) {}
-	
 }
 </SCRIPT>
 </HEAD>
 <BODY onLoad="window.focus();resize();" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
-<? 
+<?
 $picture = $_GET['pic'];
 $isindb = $_GET['db'];
 $image_id = $_GET['id'];
@@ -35,11 +60,10 @@ if(isset($picture)) {
 	} else {
 		print "<p></p><br><br><br><br><center><strong>File not found!</strong><br/><br/><input type=\"button\" value=\"Close Window\" onClick=\"window.close()\"></center>";
 	}
-	
-	
-	
-} 
+
+
+
+}
 ?>
 </BODY>
 </HTML>
- 
