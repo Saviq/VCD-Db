67ca713eb578ec5e560362eb02ce48f4c2475211
Saviq <Saviq@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2006-01-05 17:05:12 +0000

    Multi language

--- modules/footer.php
+++ modules/footer.php
@@ -1,5 +1,6 @@
 <div id="ftr" align="center">
-Page Loaded in <?=VCDUtils::getPageLoadTime()?> sec.
-(<i><?= Connection::getQueryCount()?> Queries</i>)
-&nbsp; Copyright (c) <a href="http://vcddb.konni.com" target="_new" title="Check out the official VCD-db website">VCD-db <?=VCDDB_VERSION?></a>
+<?
+printf($language->show('X_FOOTER'), VCDUtils::getPageLoadTime(), Connection::getQueryCount())
+?>
+ <a href="http://vcddb.konni.com" target="_new" title="<?=$language->show('X_FOOTER_LINK')?>">VCD-db <?=VCDDB_VERSION?></a>
 </div>
