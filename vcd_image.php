3026dde421ad9634c32445302dfd04bc2a916273
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2006-01-09 13:49:31 +0000

    Reverted to v. 1.4 header("Content-Type: image") is illegal content type. vcd_movie.php is never supposed to be called directly so it does not matter if image tries to be saved when vcd_image.php is accessed directly.

--- vcd_image.php
+++ vcd_image.php
@@ -12,10 +12,9 @@ $CLASSImage = new VCDImage($image_id);
 @ob_end_clean();
 header("Cache-Control: ");
 header("Pragma: ");
-header("Content-Type: image");
+header("Content-Type: application/octet-stream");
 header("Content-Length: " .(string)($CLASSImage->getFilesize()) );
-header('Content-Disposition: filename="'.$CLASSImage->getImageName().'"');
+header('Content-Disposition: attachment; filename="'.$CLASSImage->getImageName().'"');
 header("Content-Transfer-Encoding: binary\n");
 echo $CLASSImage->getImageStream($image_id);
 ?>
-
