1e27e0e6aa7e6e5ae3157795a81b3355b9a5bcf0
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2006-01-09 13:53:52 +0000

    CORRENT VCDConstants.php. DO Not Commit new version.

--- classes/VCDConstants.php
+++ classes/VCDConstants.php
@@ -2,20 +2,20 @@
 /**
  * VCD-db - a web based VCD/DVD Catalog system
  * Copyright (C) 2003-2004 Konni - konni.com
- *
+ * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or (at
  * your option) any later version.
- *
+ * 
  * @author  Hákon Birgsson <konni@konni.com>
  * @package Core
  * @version $Id$
  */
 ?>
-<?
+<? 
 // All constants used by VCD-db will be placed here
-define("VCDDB_VERSION","0.981");						// VCD-db version
+define("VCDDB_VERSION","0.982-CVS");					// VCD-db version
 define("STYLE","includes/templates/default/");			// Path to current template
 define("TEMP_FOLDER","upload/");						// Temp folder used by VCD-db
 define("CACHE_FOLDER","upload/cache/");					// Fetch cache folder
