b98b7aee8da413a16814c5434d0b98e55ce7a4e5
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2006-01-10 01:16:03 +0000

    *** empty log message ***

--- classes/fetch/VCDFetch.php
+++ classes/fetch/VCDFetch.php
@@ -15,19 +15,38 @@
  */
  
 ?>
-<? 
+<?
+require_once('external/snoopy/Snoopy.class.php');
+if (!defined('CACHE_FOLDER')) {
+	define("CACHE_FOLDER","upload/cache/");
+}
+
+
 abstract class VCDFetch {
 	
 	
 	protected $itemID = null;
-	protected $fetchServer;
 	
-	protected $useProxy = false;
-	protected $proxyUrl = null;
+	private $fetchDomain;
+	private $fetchSearchPath;
+	private $fetchItemPath;
+		
 	
-	protected $searchKey;
-	protected $searchContents;
-	protected $itemContents;
+	private $useProxy = false;
+	private $proxyHost;
+	private $proxyPort;
+	
+	private $searchKey;
+	private $searchContents;
+	private $itemContents;
+	
+	private $useSnoopy = false;
+	/**
+	 * Instance of Snoppy Class
+	 *
+	 * @var Snoopy
+	 */
+	private $snoopy = null;
 	
 	
 	
@@ -35,27 +54,106 @@ abstract class VCDFetch {
 		
 	}
 	
-	public function setProxyServer($strProxy) {
-		$this->proxyUrl = $strProxy;
-		$this->useProxy = true;
-	}
-	
 		
 	public abstract function search();
 	
 	public abstract function showSearchResults();
 	
+	/**
+	 * Set the fetch class to use proxy with the defined proxy parameters
+	 *
+	 * @param string $host | The proxy server hostname
+	 * @param int $port | The proxy server port
+	 */
+	protected function setProxy($host, $port) {
+		$this->proxyHost = $host;
+		$this->proxyPort = $port;
+		$this->useProxy = true;
+	}
+	
+	
+	/**
+	 * Set the correct url parameters for the current Fetch site
+	 *
+	 * @param string $domain | The full domain name of the site to fetch
+	 * @param string $searchPath | The search path without the domain name. For example "/search?text="
+	 * @param string $itemPath | The item path for specific item. For example /item/tt
+	 */
+	protected function setFetchUrls($domain, $searchPath, $itemPath) {
+		$this->fetchDomain = $domain;
+		$this->fetchSearchPath = $searchPath;
+		$this->fetchItemPath = $itemPath;
+	}
+	
 	protected function getHeader() {
 		
 	}
 	
+	
 	protected function fetchPage() {
-		
+		if ($this->useSnoopy) {
+			$this->initSnoopy();
+			
+		} else {
+						
+			
+		}
 	}
 	
 	protected function fetchCachedPage() {
 		
 		
+	}
+	
+	/**
+	 * Get the page contents from the search results
+	 *
+	 * @return string
+	 */
+	protected function getSearchContents() {
+		return $this->searchContents;		
+	}
+	
+	/**
+	 * Get the Contents of the fetched page
+	 *
+	 * @return string
+	 */
+	protected function getContents() {
+		return $this->itemContents;		
+	}
+	
+	
+	/**
+	 * Use the external Snoopy Lib to fetch the page instead of opening a socket.
+	 *
+	 */
+	protected function useSnoopy() {
+		$this->useSnoopy = true;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * Initilize a new snoopy object
+	 *
+	 */
+	private function initSnoopy() {
+		if (is_null($this->snoopy)) {
+			$this->snoopy = new Snoopy();
+			if ($this->useProxy) {
+				$this->snoopy->proxy_host = $this->proxyHost;
+				$this->snoopy->proxy_port = $this->proxyPort;
+			}
+		}
 	}
 	
 	
---
+++ classes/fetch/VCDFetch_imdb.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * VCD-db - a web based VCD/DVD Catalog system
+ * Copyright (C) 2003-2004 Konni - konni.com
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * @author  Hákon Birgsson <konni@konni.com>
+ * @package Kernel
+ * @subpackage WebFetch
+ * @version $Id$
+ */
+ 
+?>
+<? 
+class VCDFetch_imdb extends VCDFetch {
+	
+	public function __construct() {
+		
+	}
+	
+	
+	public function showSearchResults() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+?>
