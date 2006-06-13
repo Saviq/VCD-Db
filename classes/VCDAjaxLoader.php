82f64b500af5116720fc2f59d318e03ad9277c8c
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2006-06-13 01:15:23 +0000

    Initial release

---
+++ classes/VCDAjaxLoader.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * VCD-db - a web based VCD/DVD Catalog system
+ * Copyright (C) 2003-2006 Konni - konni.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * @author  Hákon Birgisson <konni@konni.com>
+ * @package Kernel
+ * @version $Id$
+ */
+?>
+<? 
+
+if (VCDUtils::isLoggedIn()) {
+
+	// Include the Ajason & Ajax libraries
+	require_once(dirname(__FILE__) . '/external/ajason/Ajax.php');
+	
+	// Create Ajax class instance
+	$ajax = new Ajax(); 
+	// Register VCD-db Ajax methods
+	$ajax->registerMethod( 'VCDXMLImporter', 'addMovie' ); 
+	$ajaxServer = $ajax->getServer(); 
+	// Check for Ajax Request and handle it.
+	if ( $ajaxServer->isRequest() ) 
+	{ 
+	  echo $ajaxServer->handleRequest(); 
+	  exit(); 
+	} 
+	$ajaxClient = $ajax->getClient(); 
+	
+	
+}
+
+
+?>
