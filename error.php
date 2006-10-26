9b6ff86ae07689c932e62243e11fc9057705c891
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2006-10-26 18:23:19 +0000

    Fixed html header

--- error.php
+++ error.php
@@ -4,7 +4,7 @@
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head><title>VCD Gallery</title>
+<head><title>VCD-db</title>
 <link rel="stylesheet" type="text/css" href="<?=STYLE?>style.css"/>
 <script src="includes/js/main.js" type="text/javascript"></script>
 </head>
