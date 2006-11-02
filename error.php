bc9d2e652d998ca4b34426ce178e5ac74af9270d
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2006-11-02 13:25:11 +0000

    Removed un-needed includes.

--- error.php
+++ error.php
@@ -1,24 +1,21 @@
-<? 
-	include_once('classes/VCDConstants.php');
-	include_once('functions/WebFunctions.php');
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head><title>VCD-db</title>
-<link rel="stylesheet" type="text/css" href="<?=STYLE?>style.css"/>
-<script src="includes/js/main.js" type="text/javascript"></script>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>VCD-db</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" type="text/css" href="includes/templates/default/style.css" media="screen, projection"/>
+	<link rel="stylesheet" type="text/css" href="includes/css/global.css" media="screen, projection"/>
 </head>
 <body>
 
-
 <h3 align="center" style="margin-right:100px">
-<img src="images/logotest.gif" width="187" align="center" height="118" alt="" border="0"/>
-<br/>
-Oops database error
+	<img src="images/logotest.gif" width="187" align="center" height="118" alt="" border="0"/>
+	<br/>
+	Oops database error
 </h3>
 
 <p align="center" style="margin-right:100px">
-
 
 <strong>Database Access misconfigured.</strong>
 <br/>
@@ -27,7 +24,7 @@ Oops database error
 	Check the <u>connection settings</u> to solve the problem.
 </span>
 <? 
-	if (checkInstall()) {
+	if (is_dir('setup')) {
 		?>
 		<br/><br/>
 		<h3 align="center" style="margin-right:100px">For a new installation <a href="setup/">go here</a></h3>
@@ -35,13 +32,6 @@ Oops database error
 	}
 ?>
 
-
-
-
-
 </p>
-
-
-
 </body>
 </html>
