588af5d1fbdefe430c1ee22782e367048cdffc1c
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2006-11-04 19:25:31 +0000

    Fixed CVS header

--- classes/pornstar/porncategoryObj.php
+++ classes/pornstar/porncategoryObj.php
@@ -8,89 +8,89 @@
  * the Free Software Foundation; either version 2 of the License, or (at
  * your option) any later version.
  * 
- * @author  Hákon Birgisson <konni@konni.com>
+ * @author  HÃ¡kon Birgisson <konni@konni.com>
  * @package Kernel
  * @subpackage Pornstars
  * @version $Id$
  */
  ?>
 <? 
-	/* 
-		Container for the Adult categories ..
-	*/
+/* 
+	Container for the Adult categories ..
+*/
 
-	class porncategoryObj implements XMLable {
+class porncategoryObj implements XMLable {
+
+	/**
+	 * Porncategory ID
+	 *
+	 * @var int
+	 */
+	private $id;
+	/**
+	 * Porncategory name
+	 *
+	 * @var string
+	 */
+	private $name;
 	
-		/**
-		 * Porncategory ID
-		 *
-		 * @var int
-		 */
-		private $id;
-		/**
-		 * Porncategory name
-		 *
-		 * @var string
-		 */
-		private $name;
-		
-		/**
-		 * Constructor, accepts array as an parameter containing all the objects variables.
-		 *
-		 * @param array $dataArr
-		 * @return porncategoryObj
-		 */
-		public function __construct($dataArr) {
-			$this->id   = $dataArr[0];
-			$this->name = $dataArr[1];
-		}
-		
-		/**
-		 * Get the object category ID
-		 *
-		 * @return int
-		 */
-		public function getID() {
-			return $this->id;
-		}
-		
-		/**
-		 * Get the object category name
-		 *
-		 * @return string
-		 */
-		public function getName() {
-			return $this->name;
-		}
-		
-		/**
-		 * Get objects id and name as array.
-		 *
-		 * Function used for example for dynamicly creating dropdownlists.
-		 *
-		 * @return array
-		 */
-		public function getList() {
-        	return array("id"   => $this->id,
-            	         "name" => $this->name);
-		}
-		
-		
-		/**
-		  * Returns the XML reprentation of the cdcover object.
-		 *
-		 * @return string
-		 */
-		public function toXML() {
-			$xmlstr  = "<category>\n";
-			$xmlstr .= "<id>".$this->id."</id>\n";
-			$xmlstr .= "<name>".$this->name."</name>\n";
-			$xmlstr .= "</category>\n";
-			
-			return $xmlstr;
-		}
-	
+	/**
+	 * Constructor, accepts array as an parameter containing all the objects variables.
+	 *
+	 * @param array $dataArr
+	 * @return porncategoryObj
+	 */
+	public function __construct($dataArr) {
+		$this->id   = $dataArr[0];
+		$this->name = $dataArr[1];
 	}
+	
+	/**
+	 * Get the object category ID
+	 *
+	 * @return int
+	 */
+	public function getID() {
+		return $this->id;
+	}
+	
+	/**
+	 * Get the object category name
+	 *
+	 * @return string
+	 */
+	public function getName() {
+		return $this->name;
+	}
+	
+	/**
+	 * Get objects id and name as array.
+	 *
+	 * Function used for example for dynamicly creating dropdownlists.
+	 *
+	 * @return array
+	 */
+	public function getList() {
+    	return array("id"   => $this->id,
+        	         "name" => $this->name);
+	}
+	
+	
+	/**
+	  * Returns the XML reprentation of the cdcover object.
+	 *
+	 * @return string
+	 */
+	public function toXML() {
+		$xmlstr  = "<category>\n";
+		$xmlstr .= "<id>".$this->id."</id>\n";
+		$xmlstr .= "<name>".$this->name."</name>\n";
+		$xmlstr .= "</category>\n";
+		
+		return $xmlstr;
+	}
+
+}
 
 
 ?>
