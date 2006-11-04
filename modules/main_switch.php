87b3742676d71930bacb101f4164fb29e68b1e4b
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2006-11-04 23:01:29 +0000

    Changed all include directives to use VCDDB_BASE

--- modules/main_switch.php
+++ modules/main_switch.php
@@ -11,7 +11,7 @@ switch ($CURRENT_PAGE) {
 
 	case 'category':
 		if (is_numeric($_GET['category_id'])) {
-			require_once('pages/category.php');
+			require_once(VCDDB_BASE.'/pages/category.php');
 		} else {
 			redirect();
 			exit();
@@ -23,10 +23,10 @@ switch ($CURRENT_PAGE) {
 		if ($SETTINGSClass->getSettingsByKey('SITE_ADULT')) {
 			
 			if (isset($_GET['category_id'])) {
-				require_once('pages/adultcategory.php');
+				require_once(VCDDB_BASE.'/pages/adultcategory.php');
 				break;
 			} elseif (isset($_GET['studio_id'])) {
-				require_once('pages/adultstudio.php');
+				require_once(VCDDB_BASE.'/pages/adultstudio.php');
 				break;
 			} else {
 				redirect();
@@ -42,29 +42,29 @@ switch ($CURRENT_PAGE) {
 		
 	case 'register':
 		if (LDAP_AUTH != 1) {
-			require_once('pages/register.php');
+			require_once(VCDDB_BASE.'/pages/register.php');
 		} else {
 			redirect();
 		}
 		break;
 		
 	case 'welcome':
-		require_once('pages/welcome.php');
+		require_once(VCDDB_BASE.'/pages/welcome.php');
 		break;
 		
 		
 	case 'badlogin':
-		require_once('pages/login_error.php');
+		require_once(VCDDB_BASE.'/pages/login_error.php');
 		break;
 		
 	case 'reset':
-		require_once('pages/newpass.php');
+		require_once(VCDDB_BASE.'/pages/newpass.php');
 		break;
 		
 	case 'cd':
 		if (is_numeric($_GET['vcd_id'])) {
 			global $showright;
-			require_once('pages/vcd.php');
+			require_once(VCDDB_BASE.'/pages/vcd.php');
 		} else {
 			redirect();
 			exit();
@@ -72,27 +72,19 @@ switch ($CURRENT_PAGE) {
 		break;
 		
 	case 'detailed_search':
-		require_once('pages/detailed_search.php');
+		require_once(VCDDB_BASE.'/pages/detailed_search.php');
 		break;
 		
 	case 'pornstars':
-		require_once('pages/view_pornstars.php');
+		require_once(VCDDB_BASE.'/pages/view_pornstars.php');
 		break;
 		
-			
-	case 'langdump':
-		ob_get_clean();
-		ob_start();
-		print_r($language->getLangDump($_GET['i']));
-		ob_flush();
-		exit();
-		break;
 		
 	case 'pornstar':
 		if (is_numeric($_GET['pornstar_id'])) {
 			// is this permitted by the administrator ?
 			if ($SETTINGSClass->getSettingsByKey('SITE_ADULT')) {
-				require_once('pages/pornstar.php');
+				require_once(VCDDB_BASE.'/pages/pornstar.php');
 			} else {
 				redirect();
 				exit();
@@ -115,47 +107,47 @@ switch ($CURRENT_PAGE) {
 		
 			switch ($OPERATION) {
 				case 'settings':
-					require_once('pages/edit_user.php');
+					require_once(VCDDB_BASE.'/pages/edit_user.php');
 				break;
 
 				case 'new':
-					require_once('pages/add_movie.php');
+					require_once(VCDDB_BASE.'/pages/add_movie.php');
 				break;
 				
 				case 'add_manually':
-					require_once('pages/add_manually.php');
+					require_once(VCDDB_BASE.'/pages/add_manually.php');
 				break;
 				
 				case 'add_listed':
-					require_once('pages/add_listed.php');
+					require_once(VCDDB_BASE.'/pages/add_listed.php');
 				break;
 				
 				case 'movies':
-					require_once('pages/my_movies.php');
+					require_once(VCDDB_BASE.'/pages/my_movies.php');
 				break;
 				
 				case 'wishlist':
-					require_once('pages/wishlist.php');
+					require_once(VCDDB_BASE.'/pages/wishlist.php');
 				break;
 				
 				case 'publicwishlist':
-					require_once('pages/wishlistpublic.php');
+					require_once(VCDDB_BASE.'/pages/wishlistpublic.php');
 				break;
 				
 				case 'rss':
-					require_once('pages/my_rssfeeds.php');
+					require_once(VCDDB_BASE.'/pages/my_rssfeeds.php');
 				break;
 				
 				case 'stats':
-					require_once('pages/user_statistics.php');
+					require_once(VCDDB_BASE.'/pages/user_statistics.php');
 				break;
 				
 				
 				case 'loans':
 					if (isset($_GET['history'])) {
-						require_once('pages/loanhistory.php');
+						require_once(VCDDB_BASE.'/pages/loanhistory.php');
 					} else {
-						require_once('pages/loans.php');
+						require_once(VCDDB_BASE.'/pages/loans.php');
 					}
 				break;
 				
@@ -167,35 +159,35 @@ switch ($CURRENT_PAGE) {
 						$source = $_GET['source'];
 						
 						if (strcmp($source, "webfetch") == 0) {
-							require_once('pages/add_webfetch.php');
+							require_once(VCDDB_BASE.'/pages/add_webfetch.php');
 						}
 						
 						if (strcmp($source, "moviefetch") == 0) {
-							require_once('pages/add_imdbfetch.php');
+							require_once(VCDDB_BASE.'/pages/add_imdbfetch.php');
 						}
 						
 						if (strcmp($source, "adultmoviefetch") == 0) {
-							require_once('pages/add_empirefetch.php');
+							require_once(VCDDB_BASE.'/pages/add_empirefetch.php');
 						}
 						
 						if (strcmp($source, "xmlresults") == 0) {
-							require_once('pages/xml_results.php');
+							require_once(VCDDB_BASE.'/pages/xml_results.php');
 						}
 						
 						if (strcmp($source, "xml") == 0) {
-							require_once('pages/xml_confirm.php');
+							require_once(VCDDB_BASE.'/pages/xml_confirm.php');
 						}
 						
 						if (strcmp($source, "excelresults") == 0) {
-							require_once('pages/excel_results.php');
+							require_once(VCDDB_BASE.'/pages/excel_results.php');
 						}
 						
 						if (strcmp($source, "excel") == 0) {
-							require_once('pages/excel_confirm.php');
+							require_once(VCDDB_BASE.'/pages/excel_confirm.php');
 						}
 						
 						if (strcmp($source, "listed") == 0) {
-							require_once('pages/listed_confirm.php');
+							require_once(VCDDB_BASE.'/pages/listed_confirm.php');
 						}
 						
 						
@@ -218,17 +210,9 @@ switch ($CURRENT_PAGE) {
 		
 	default:
 		
-		include('frontpage.php');
+		include(dirname(__FILE__).'/frontpage.php');
 		break;
 
 }
-
-
-
-
-
-
-
-
 
 ?>
