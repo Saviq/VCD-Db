1826b8c1723ad2087e1c7649d79fe53916882939
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2006-11-04 13:35:23 +0000

    CVS header fixed.

--- classes/VCDClassFactory.php
+++ classes/VCDClassFactory.php
@@ -8,7 +8,7 @@
  * the Free Software Foundation; either version 2 of the License, or (at
  * your option) any later version.
  * 
- * @author  H�kon Birgsson <konni@konni.com>
+ * @author  Hákon Birgisson <konni@konni.com>
  * @package Kernel
  * @version $Id$
  */
