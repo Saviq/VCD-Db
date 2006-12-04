607cbb1e2ded1bd27692bc75aa7d45b58dc0eb37
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2006-12-04 18:05:24 +0000

    Draft of the amazon.com fetch class.

---
+++ classes/fetch/VCDFetch_amazon.php
@@ -0,0 +1,229 @@
+<?php
+/**
+ * VCD-db - a web based VCD/DVD Catalog system
+ * Copyright (C) 2003-2006 Konni - konni.com
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * @author  Hákon Birgisson <konni@konni.com>
+ * @package Kernel
+ * @subpackage WebFetch
+ * @version $Id$
+ */
+ 
+?>
+<? 
+class VCDFetch_amazon extends VCDFetch {
+	
+	
+	protected $regexArray = array(
+		'title' 	  => '<b class="sans">([^<]*)</b><br />',
+		'year'  	  => 'REL: ([0-9]{1,2})/([0-9]{1,2})/([0-9]{4})<br>',
+		'studio'	  => 'Manufacturer=([^<]*)>([^<]*)</a><BR>',
+		'screens'	  => 'topoftabs\">([^<]*) Screen Shots</a>',
+		'genre'	 	  => 'CAT: <a href=search_result.asp?([^<]*)>([^<]*)</a><BR>',
+		'cast' 		  => '</font><font color=#00000>([^<]*)<br>',
+		'thumbnail'	  => null,
+		'frontcover'  => null,
+		'backcover'   => null
+		);
+	
+			
+	protected $multiArray = array(
+		'cast', 'genre', 'poster'
+	);
+		
+		
+		
+	private $servername = 'www.amazon.com';
+	private $searchpath = '/s/102-1930070-8444921?url=search-alias%3Ddvd&field-keywords=[$]';
+	private $itempath   = '/dp/[$]';
+		
+	
+	public function __construct() {
+		$this->setSiteName("amazon");
+		$this->setFetchUrls($this->servername, $this->searchpath, $this->itempath);
+	}
+	
+	protected function processResults() {
+			if (!is_array($this->workerArray) || sizeof($this->workerArray) == 0) {
+				$this->setErrorMsg("No results to process.");
+				return;
+			}
+					
+		$obj = new imdbObj();
+		$obj->setObjectID($this->getItemID());
+		
+				
+		foreach ($this->workerArray as $key => $data) {
+			
+			$entry = $data[0];
+			$arrData = $data[1];
+			
+			switch ($entry) {
+				case 'title':
+					$title = $arrData[1];
+					$regex = "\(([A-z]{3})\)";
+					$title = ereg_replace($regex, "", $title);
+					$obj->setTitle($title);
+					break;
+				
+				case 'year':
+					$year = $arrData[3];
+					$obj->setYear($year);		
+					break;
+					
+				case 'studio':
+					$studio = $arrData[2];
+					$obj->setStudio($studio);
+					break;
+										
+				case 'thumbnail':
+					$obj->setImage($arrData);
+					break;
+					
+				case 'genre':
+					foreach ($arrData as $item) {
+						$genre = $item[2];
+						$obj->addCategory($genre);
+					}
+					break;
+					
+				case 'cast':
+					if (isset($arrData[0][1])) {
+						$pornstars = explode(",", $arrData[0][1]);
+						foreach ($pornstars as $pornstar) {
+							$obj->addActor(ltrim($pornstar, "\."));
+						}
+					}
+					
+					
+					break;
+					
+					
+				default:
+					break;
+			}
+			
+		}
+		
+		$this->fetchedObj = $obj;
+	}
+	
+	
+	protected function fetchDeeper($entry) {
+		
+		switch ($entry) {
+			case 'thumbnail':
+				$value = $this->getImagePath($entry);
+				array_push($this->workerArray, array($entry, $value));
+				break;
+				
+			case 'frontcover':
+				$value = $this->getImagePath($entry);
+				array_push($this->workerArray, array($entry, $value));
+				break;
+				
+			case 'backcover':
+				$value = $this->getImagePath($entry);
+				array_push($this->workerArray, array($entry, $value));
+				break;
+				
+			case 'screenshots':
+				$value = $this->getImagePath($entry);
+				array_push($this->workerArray, array($entry, $value));
+				break;
+		
+			default:
+				break;
+		}
+		
+	}
+	
+	public function search($title) { 
+		return parent::search($title);
+	}
+	
+	public function showSearchResults() {
+		
+		$this->setMaxSearchResults(50);
+		$regx = '<a href="http://www.amazon.com/([^<]*)"><span class="srTitle">([^<]*)</span></a>';
+
+		$results = parent::generateSimpleSearchResults($regx,1,2);
+		
+		parent::generateSearchSelection($results);
+					
+	}
+	
+	
+	/**
+	 * Get the Full HTTP image path for the asked for image on the JadedVideo server.
+	 * Valid image types are thumbnail, frontcover, backcover.
+	 * All except screenshots return strings, screenshots returns an array of all screenshot images for that movie.
+	 *
+	 * @param string $image_type
+	 * @return mixed.
+	 */
+	private function getImagePath($image_type, $fallback = 0) {
+	
+		if ($fallback == 0) {
+			$folder = substr($this->getItemID(),0,3);
+		} elseif ($fallback == 1) {
+			$folder = substr($this->getItemID(),0,2);
+		} elseif ($fallback == 2) {
+			$folder = substr($this->getItemID(),0,1);
+		} else {
+			return null;
+		}
+		
+						
+		switch ($image_type) {
+			case 'thumbnail':
+				$imagebase = "http://www.jadedvideo.com/imagesjaded/".$folder."/thumbs/".$this->getItemID();
+				$fileurl = $imagebase.".jpg";
+				if ($this->remote_file_exists($fileurl)) {
+					return $fileurl;
+				} else {
+					return $this->getImagePath($image_type, $fallback+1);
+				}
+				break;
+				
+			case 'frontcover':
+				$imagebase = "http://www.jadedvideo.com/imagesjaded/".$folder."/front/".$this->getItemID();
+				$fileurl = $imagebase.".jpg";
+				if ($this->remote_file_exists($fileurl)) {
+					return $fileurl;
+				} else {
+					return $this->getImagePath($image_type, $fallback+1);
+				}
+				break;
+				
+			case 'backcover':
+				$imagebase = "http://www.jadedvideo.com/imagesjaded/".$folder."/back/".$this->getItemID();
+				$fileurl = $imagebase.".jpg";
+				if ($this->remote_file_exists($fileurl)) {
+					return $fileurl;
+				} else {
+					return $this->getImagePath($image_type, $fallback+1);
+				}
+				break;
+						
+			default:
+				return false;
+				break;
+		}
+	
+	}
+	
+	
+	
+	
+}
+
+
+
+
+?>
