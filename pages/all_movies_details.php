2b6cf36be4a38909463cd222e71b997e262d2495
fouff <fouff@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2007-02-12 18:20:01 +0000

    The 2 new files for idsplaying popups

---
+++ pages/all_movies_details.php
@@ -0,0 +1,177 @@
+<?php  
+	require_once("../classes/includes.php");
+	if (!VCDUtils::isLoggedIn()) {
+		VCDException::display("User must be logged in");
+		print "<script>self.close();</script>";
+		exit();
+	}
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>VCD Gallery</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo VCDUtils::getCharSet() ?>"/>
+	<link rel="stylesheet" type="text/css" href="../<?php echo VCDUtils::getStyle() ?>"/>
+	<script src="../includes/js/main.js" type="text/javascript"></script>
+</head>
+<body class="nobg">
+
+
+
+<?php 
+   $s = new vcd_settings();
+   $v = VCDClassFactory::getInstance("vcd_movie");
+   $categories = $s->getMovieCategoriesInUse();
+   if (sizeof($categories) == 0)
+   {
+      VCDException::display('You have not added any movies yet<break>Try again after you have inserted some movies');
+      print "<script>window.close()</script>";
+      exit();
+   }
+   
+   //Get categories names
+   $newCatName = array();
+   foreach ($categories as $categoryObj)
+   {
+      $mapping = getCategoryMapping();
+      if(isset($mapping[$categoryObj->getName()]))
+      {
+         $translated = $mapping[$categoryObj->getName()];
+         $newKey = VCDLanguage::translate($translated);
+      }
+      else
+         $newKey = $categoryObj->getName();
+      array_push($newCatName, $newKey);
+   }
+   //Alphabetical sort catégories names
+   sort($newCatName);
+   //Reorder categories objects according to categories names
+   $newCatObj = array();
+   foreach ($newCatName as $name)
+   {
+      foreach ($categories as $categoryObj)
+      {
+         $mapping = getCategoryMapping();
+         if(isset($mapping[$categoryObj->getName()]))
+         {
+            $translated = $mapping[$categoryObj->getName()];
+            $newKey = VCDLanguage::translate($translated);
+         }
+         else
+            $newKey = $categoryObj->getName();
+         if($newKey == $name)
+         {
+            array_push($newCatObj, $categoryObj);
+            break;
+         }
+      }
+   }
+   $categories = $newCatObj;
+   
+   $arr = $s->getMediaTypesInUse();
+   $arrMediaTypes = array();
+   $arrMediaTypeNames = array();
+   $mediaTypesCount = 0;
+   
+   print "<table cellspacing=\"1\" cellpadding=\"1\" border=\"0\" width=\"100%\">";
+   print "<tr><td>&nbsp;</td>";
+   foreach ($arr as $item)
+   {
+      print "<td nowrap=\"noswrap\" class=\"statc\">".$item[1] . "</td>";
+      array_push($arrMediaTypes, $item[0]);
+      array_push($arrMediaTypeNames, $item[1]);
+      $mediaTypesCount = $mediaTypesCount + 1;
+   }
+   print "<td class=\"statc\">&nbsp;</td>";
+   print "</tr>";
+   
+   $arrMediaTypes = array_flip($arrMediaTypes);
+   $mod = 0;
+   foreach ($categories as $categoryObj)
+   {
+      $css_class_cat = "statc";
+      
+      //Translate category name to be displayed
+      $mapping = getCategoryMapping();
+      if(isset($mapping[$categoryObj->getName()]))
+      {
+         $translated = $mapping[$categoryObj->getName()];
+         $newKey = VCDLanguage::translate($translated);
+      }
+      else
+         $newKey = $$categoryObj->getName();
+      
+      print "<tr>";
+      print "<td nowrap=\"nowrap\" class=\"".$css_class_cat."\">".$newKey."</td>";
+      $arr2 = $s->getMediaCountByCategory($categoryObj->getID());
+      $resultArr = getCategoryResults($arrMediaTypes,  $arr2);
+      $category_sum = 0;
+      foreach ($resultArr as $count)
+      {
+         print "<td align=\"right\" class=\"".$css_class_cat."\">$count</td>";
+         $category_sum += $count;
+      }
+      print "<td align=\"right\" class=\"".$css_class_cat."\"><b>".$category_sum."</b></td>";
+      print "</tr>";
+      
+      //To print movies name inside current category
+      $batch  = 0;
+      if (isset($_GET['batch']))
+         $batch = $_GET['batch'];
+      $Recordcount = $s->getSettingsByKey("PAGE_COUNT");
+      $offset = $batch*$Recordcount;
+      $movies = $v->getVcdByCategory($categoryObj->getID(), $Recordcount, $offset);
+      $lig = 0;
+      foreach ($movies as $movie)
+      {
+         if(($lig % 2) == 0)
+            $css_class_movie = "statb";
+         else
+            $css_class_movie = "stata";
+         
+         print "<tr>";
+         print "<td nowrap=\"nowrap\" class=\"".$css_class_movie."\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".$movie->getTitle()."</td>";
+         $lastFound = "";
+         foreach ($resultArr as $count)
+         {
+            $stringToWrite = "&nbsp;";
+            $mediaType = $movie->getMediaType();
+            foreach($mediaType as $mediaTypeObj)
+            {
+               if($mediaTypeObj->getName() == $lastFound)
+                  continue ;
+               if(in_array($mediaTypeObj->getName(), $arrMediaTypeNames) and $mediaTypesCount > 1)
+               {
+                  $lastFound = $mediaTypeObj->getName();
+                  $stringToWrite = " X ";
+                  break;
+               }
+            }
+            print "<td align=\"center\" class=\"".$css_class_movie."\">$stringToWrite</td>";
+         }
+         print "</tr>";
+         
+         $lig++;
+      }
+      $mod++;
+   }
+   
+   print "<tr>";
+   print "<td class=\"statc\">&nbsp;</td>";
+   $total_sum = 0;
+   foreach ($arr as $results_count)
+   {
+      $total_sum += $results_count[2];
+      print "<td class=\"statc\" align=\"right\"><b>".$results_count[2]."</b></td>";
+   }
+   print "<td class=\"statc\" align=\"right\"><b>".$total_sum."</b></td>";
+   print "</tr>";
+   print "</table>";
+?>
+
+   <br/><hr/>
+   <p align="center"><input onclick="window.close()" type="button" value="<?php echo VCDLanguage::translate('misc.close')?>"/></p>
+
+</body>
+</html>
---
+++ pages/user_status_detail.php
@@ -0,0 +1,145 @@
+<?php 
+	include_once("../classes/includes.php");
+	if (!VCDUtils::isLoggedIn()) {
+		die('Login first to use this page');
+	}
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>VCD-db</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo VCDUtils::getCharSet()?>"/>
+	<link rel="stylesheet" type="text/css" href="../<?php echo STYLE?>style.css"/>
+	<script src="../includes/js/main.js" type="text/javascript"></script>
+</head>
+<body class="nobg">
+
+
+<?php 
+
+   $s = new vcd_settings();
+   $v = VCDClassFactory::getInstance("vcd_movie");
+   $userCategories = $s->getCategoriesInUseByUserID(VCDUtils::getUserID());
+   if (sizeof($userCategories) == 0)
+   {
+      VCDException::display('You have not added any movies yet<break>Try again after you have inserted some movies');
+      print "<script>window.close()</script>";
+      exit();
+   }
+   
+   //Get categories names
+   $newCatName = array();
+   foreach ($userCategories as $categoryObj)
+   {
+      $mapping = getCategoryMapping();
+      if(isset($mapping[$categoryObj->getName()]))
+      {
+         $translated = $mapping[$categoryObj->getName()];
+         $newKey = VCDLanguage::translate($translated);
+      }
+      else
+         $newKey = $categoryObj->getName();
+      array_push($newCatName, $newKey);
+   }
+   //Alphabetical sort catégories names
+   sort($newCatName);
+   //Reorder categories objects according to categories names
+   $newCatObj = array();
+   foreach ($newCatName as $name)
+   {
+      foreach ($userCategories as $categoryObj)
+      {
+         $mapping = getCategoryMapping();
+         if(isset($mapping[$categoryObj->getName()]))
+         {
+            $translated = $mapping[$categoryObj->getName()];
+            $newKey = VCDLanguage::translate($translated);
+         }
+         else
+            $newKey = $categoryObj->getName();
+         if($newKey == $name)
+         {
+            array_push($newCatObj, $categoryObj);
+            break;
+         }
+      }
+   }
+   $userCategories = $newCatObj;
+   
+   $arr = $s->getMediaTypesInUseByUserID(VCDUtils::getUserID());
+   $arrMediaTypes = array();
+   
+   print "<table cellspacing=\"1\" cellpadding=\"1\" border=\"0\" width=\"100%\">";
+   print "<tr><td>&nbsp;</td>";
+   foreach ($arr as $item)
+   {
+      print "<td nowrap=\"noswrap\" class=\"stata\">".$item[1] . "</td>";
+      array_push($arrMediaTypes, $item[0]);
+   }
+   print "<td class=\"stata\">&nbsp;</td>";
+   print "</tr>";
+   
+   $arrMediaTypes = array_flip($arrMediaTypes);
+   $mod = 0;
+   foreach ($userCategories as $categoryObj)
+   {
+      $css_class = "stata";
+      
+      // Translate category name
+      $mapping = getCategoryMapping();
+      if(isset($mapping[$categoryObj->getName()]))
+      {
+         $translated = $mapping[$categoryObj->getName()];
+         $newKey = VCDLanguage::translate($translated);
+      }
+      else
+         $newKey = $$categoryObj->getName();
+      
+      print "<tr>";
+      print "<td nowrap=\"nowrap\" class=\"".$css_class."\">".$newKey."</td>";
+      $arr2 = $s->getMediaCountByCategoryAndUserID(VCDUtils::getUserID(),$categoryObj->getID());
+      $resultArr = getCategoryResults($arrMediaTypes,  $arr2);
+      $category_sum = 0;
+      foreach ($resultArr as $count)
+      {
+         print "<td class=\"".$css_class."\" align=\"right\">$count</td>";
+         $category_sum += $count;
+      }
+      print "<td align=\"right\" class=\"".$css_class."\"><b>".$category_sum."</b></td>";
+      print "</tr>";
+      
+      // Print movies name inside current category
+      $batch  = 0;
+      if (isset($_GET['batch']))
+         $batch = $_GET['batch'];
+      $Recordcount = $s->getSettingsByKey("PAGE_COUNT");
+      $offset = $batch*$Recordcount;
+      $movies = $v->getVcdByCategory($categoryObj->getID(), $Recordcount, $offset, VCDUtils::getUserID());
+      foreach ($movies as $movie)
+      {
+         print "<tr>";
+         print "<td nowrap=\"nowrap\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".$movie->getTitle()."</td>";
+         print "</tr>";
+      }
+      $mod++;
+   }
+   
+   print "<tr>";
+   print "<td class=\"stata\">&nbsp;</td>";
+   $total_sum = 0;
+   foreach ($arr as $results_count)
+   {
+      $total_sum += $results_count[2];
+      print "<td class=\"stata\" align=\"right\"><b>".$results_count[2]."</b></td>";
+   }
+   print "<td class=\"stata\" align=\"right\"><b>".$total_sum."</b></td>";
+   print "</tr>";
+   print "</table>";
+?>
+
+   <br/><hr/>
+   <p align="center"><input onclick="window.close()" type="button" value="<?php echo VCDLanguage::translate('misc.close')?>"/></p>
+
+</body>
+</html>
