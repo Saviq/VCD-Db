776577006335a99b55622b37186f46ef80fed6ae
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2007-02-19 01:55:27 +0000

    Still a work in process

--- classes/pornstar/pornstarUpdater.php
+++ classes/pornstar/pornstarUpdater.php
@@ -4,14 +4,62 @@ require_once(VCDDB_BASE.'/classes/external/nusoap.php');
 class PornstarProxy {
 
 	//static private $wsdl = "http://bis-konnz:88/projects/vcddb-ws/index.php?wsdl";
-	static private $wsdl = "http://konni/vcddb-ws/index.php?wsdl";
-	
+	//static private $wsdl = "http://konni/vcddb-ws/index.php?wsdl";
+	static private $currCollection;
 	
 	static public function doHandshake() {
 		try {
+			
+			// Get the endpoint
+			self::discoverServiceUri();
+			
+			// Report success
+			return "Got Service: " . self::getWSDL();
 		
 		} catch (Exception $ex) {
 			throw new AjaxException($ex->getMessage(), $ex->getCode());
+		}
+	}
+
+	/**
+	 * Discover the SOAP Service from the vcddb.konni.com website.
+	 * And then store the wsdl url in session
+	 *
+	 */
+	static private function discoverServiceUri() {
+		try {
+			
+			$base = "http://vcddb.konni.com/ws.vcddb.xml";
+			$xml = simplexml_load_file($base);
+		
+			if (!$xml) {
+				throw new Exception('Could not locate WSDL uri, try again later.');
+			}
+		
+			$wsdluri = (string)$xml->wsdl;
+			$_SESSION['vcddb-wsdl']	= $wsdluri;
+						
+		} catch (Exception $ex) {
+			throw $ex;
+		}
+	}
+	
+	/**
+	 * Get the Pornstar Sync Server Endpoint
+	 *
+	 * @return string | The WSDL endpoint url
+	 */
+	static private function getWSDL() {
+		try {
+			
+			if (!isset($_SESSION['vcddb-wsdl'])) {
+				throw new Exception('No Service Endpoint has been specified');
+			} else {
+				return $_SESSION['vcddb-wsdl'];
+			}
+			
+		} catch (Exception $ex) {
+			throw $ex;
 		}
 	}
 	
@@ -25,8 +73,13 @@ class PornstarProxy {
 		try {
 		
 			
-			$Updater = new pornstarUpdater(self::$wsdl);
+			$Updater = new pornstarUpdater(self::getWSDL());
 			$list = $Updater->getListByLetter($letter);
+			
+			// Store the list in session
+			self::storeList($list);
+			
+			//VCDUtils::write('/home/konni/www/vcddb/upload/'.$letter.'.txt', print_r($list, true));
 			
 			$totalSize = 0;
 			foreach ($list as $arr)	{ $totalSize += sizeof($arr);}
@@ -57,9 +110,64 @@ class PornstarProxy {
 	static public function getUpdates($index) {
 		try {
 		
+			
+			// Get the list from session
+			$list = self::retrieveList();
+			
+			
+			$arrListTypes = array('incoming','outgoing','serverupdate','clientupdate','clientserverupdate');
+			$listIndex = 0;
+			foreach ($arrListTypes as $listType) {
+				$currArray = $list[$listType];
+				for ($i=0;$i<sizeof($currArray);$i++,$listIndex++) {
+					if ($index == $listIndex){
+						$Updater = new pornstarUpdater(self::getWSDL());
+						return $Updater->processSyncRequest($listType, $currArray[$i]);
+						//return "Action: " . $listType . " pornstar: " . $currArray[$i]['name'];
+					}
+				}
+			}
+			
+			throw new Exception('Requested index not found');
+
+			
 		} catch (Exception $ex) {
 			throw new AjaxException($ex->getMessage(), $ex->getCode());
 		}		
+	}
+	
+	/**
+	 * Store the current SOAP results in memory
+	 *
+	 * @param array $soaplist | The incoming SOAP message
+	 */
+	static private function storeList($soaplist) {
+		try {
+			
+			$_SESSION['starCollection'] = $soaplist;
+						
+		} catch (Exception $ex) {
+			throw $ex;
+		}
+	}
+	
+	/**
+	 * Get the current SOAP message from memory
+	 *
+	 * @return array 
+	 */
+	static private function retrieveList() {
+		try {
+			
+			if (!isset($_SESSION['starCollection'])) {
+				throw new Exception('No list found in memory');
+			} else  {
+				return $_SESSION['starCollection'];
+			}
+			
+		} catch (Exception $ex) {
+			throw $ex;
+		}
 	}
 }
 
@@ -73,7 +181,7 @@ class pornstarUpdater {
 	 */
 	private $soapClient;
 	//private $letters = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
-	private $letters = array('A');
+	//private $letters = array('A');
 	
 	
 	
@@ -87,6 +195,7 @@ class pornstarUpdater {
 	public function getListByLetter($letter) {
 		try {
 		
+			PornstarServices::disableErrorHandler();
  			$stars = PornstarServices::getPornstarsByLetter($letter, false);
  			$soapList = $this->createListRequest($stars);
  			
@@ -103,6 +212,136 @@ class pornstarUpdater {
 		
 	}
 	
+	public function processSyncRequest($action, $pornstarData) {
+		try {
+			
+			$param = $this->prepareSyncRequest($action, $pornstarData);
+			
+			switch ($action) {
+				case 'incoming':
+					$response = $this->soapClient->call('GetPornstarByName', $param);
+					$obj = new pornstarObj(array('',$response['name'], $response['website'], '', $response['biography']));
+										
+					
+					$imgData = $response['image'];
+					// Check if this is really an image
+					if (strlen($imgData) > 10) {
+						//VCDUtils::write('/home/konni/www/vcddb/upload/images.txt', $obj->getName() . base64_decode($imgData) . "\n", true);
+						$imagename = VCDUtils::generateUniqueId().'.jpg';
+						if ($this->writeImage(BASE.DIRECTORY_SEPARATOR.PORNSTARIMAGE_PATH.$imagename,  $imgData)) {
+							$obj->setImageName($imagename);
+						}
+					}
+					
+					
+					
+					PornstarServices::disableErrorHandler();
+					PornstarServices::addPornstar($obj);
+					return 'incoming ' . $obj->getName();
+					
+					break;
+					
+				case 'outgoing':
+					
+					VCDUtils::write('/home/konni/www/vcddb/upload/sending.txt', print_r($param, true) , true);
+					
+					$response = $this->soapClient->call('AddPornstar', $param);
+					if ($response == true) {
+						return 'outgoing: successfully sent ' . $pornstarData['name'] . ' to master server';
+					} else {
+						return 'outgoing: failed to send ' . $pornstarData['name'] . ' to master server';
+					}
+					break;
+					
+				case 'serverupdate':
+					
+					break;
+			
+				case 'clientupdate':
+					$response = $this->soapClient->call('GetPornstarByName', $param);
+					$localObj = PornstarServices::getPornstarByName($pornstarData['name']);
+					
+					$updateList = array();
+					
+					if ($pornstarData['biography'] == "1") {
+						$localObj->setBiography($response['biography']);
+						$updateList[] = 'biography';
+					}
+					
+					if ($pornstarData['website'] == "1") {
+						$localObj->setHomePage($response['website']);
+						$updateList[] = 'website';
+					}
+					
+					if ($pornstarData['image'] == "1") {
+						$imgData = $response['image'];
+						// Check if this is really an image
+						if (strlen($imgData) > 10) {
+							$imagename = VCDUtils::generateUniqueId().'.jpg';
+							if ($this->writeImage(BASE.DIRECTORY_SEPARATOR.PORNSTARIMAGE_PATH.$imagename,  $imgData)) {
+								$localObj->setImageName($imagename);
+							}
+							$updateList[] = 'image';
+						}
+					}
+					
+					//VCDUtils::write('/home/konni/www/vcddb/upload/'.$pornstarData['name'].'.txt', print_r($pornstarData, true));
+					
+					return 'clientupdate ' . $localObj->getName() . ' updates: ' . implode(',', $updateList);
+					break;
+					
+				case 'clientserverupdate':
+					
+					break;
+					
+				default: throw new Exception('Undefined action:' + $action);
+			}
+			
+			
+			
+		} catch (Exception $ex) {
+			throw $ex;
+		}
+	}
+	
+	private function prepareSyncRequest($action, $data) {
+		try {
+			
+			switch ($action) {
+				case 'incoming':
+				case 'clientupdate':
+					return array('PornstarName' => $data['name']);
+					break;
+					
+				case 'outgoing':
+					PornstarServices::disableErrorHandler();
+					$pornstarObj = PornstarServices::getPornstarByName($data['name']);
+					$param = array(
+						'name' => utf8_encode($pornstarObj->getName()),
+						'biography' =>  utf8_encode($pornstarObj->getBiography()),
+						'website' => utf8_encode($pornstarObj->getHomepage()),
+						'image' => base64_encode(file_get_contents(BASE.DIRECTORY_SEPARATOR.PORNSTARIMAGE_PATH.$pornstarObj->getImageName()))
+						);
+					return array('Pornstar' => $param);
+					break;
+					
+				case 'serverupdate':
+					
+					break;
+			
+					
+				case 'clientserverupdate':
+					
+					break;
+					
+				default: throw new Exception('Undefined action:' + $action);
+			}
+			
+		} catch (Exception $ex) {
+			throw $ex;
+		}
+		
+	}
 	
 	private function handleListResponse($arrServerResults) {
 		try {
@@ -158,23 +397,23 @@ class pornstarUpdater {
 						
 						switch ($responseItem['action']) {
 							case 'getFromServer':
-								$arrIncoming[] = $responseItem['name'];
+								$arrIncoming[] = $responseItem;
 								break;
 								
 							case 'sendToServer':
-								$arrOutgoing[] = $responseItem['name'];
+								$arrOutgoing[] = $responseItem;
 								break;
 														
 							case 'clientserverUpdate':
-								$arrClientServerUpdate[] = $responseItem['name'];
+								$arrClientServerUpdate[] = $responseItem;
 								break;
 								
 							case 'clientUpdate':
-								$arrClientUpdate[] = $responseItem['name'];
+								$arrClientUpdate[] = $responseItem;
 								break;
 								
 							case 'serverUpdate':
-								$arrServerUpdate[] = $responseItem['name'];
+								$arrServerUpdate[] = $responseItem;
 								break;
 						
 							default: break;
@@ -233,6 +472,39 @@ class pornstarUpdater {
 		
 	}
 	
+	
+	/**
+	 * Write the image thumbnail to disk
+	 *
+	 * @param string $filename | The image filename
+	 * @param string $content | The file data
+	 * @return bool | returns true if successful
+	 */
+	private function writeImage($filename, $content) {
+		try {
+			
+			if (empty($filename)) {
+				throw new Exception('No filename given for target file.');
+			}
+			
+			if (empty($content)) {
+				throw new Exception('cant write to file: ' . $filename ." , no content specified.");
+			}
+		
+			$fp = fopen($filename,"w");
+			$b = fwrite($fp,$content);
+			fclose($fp);
+			if($b != -1){
+				return true;
+			} else {
+				throw new Exception('Could not write to disk, check permissons');
+			}
+			
+			
+		} catch (Exception $ex) {
+			throw $ex;
+		}
+	}
 	
 	
 	
