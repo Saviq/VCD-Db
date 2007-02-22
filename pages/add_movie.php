8eb568c979c6750ccd7d8667e8b8d0ebaf689fc1
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2007-02-22 15:19:19 +0000

    Updated the UI for the page

--- pages/add_movie.php
+++ pages/add_movie.php
@@ -1,45 +1,36 @@
 <h1><?=VCDLanguage::translate('menu.addmovie')?></h1>
-&nbsp;&nbsp;<strong class="plain"><?=VCDLanguage::translate('addmovie.info')?></strong>
-<br/>
-<br/>
-<h2>A) <?=VCDLanguage::translate('addmovie.imdb')?></h2>
-<ul>
 
+<fieldset id="pzone" title="<?=VCDLanguage::translate('menu.addmovie')?>">
+<legend class="bold"><?=VCDLanguage::translate('addmovie.info')?></legend>
+
+<p class="outer">
+<img src="images/icons/transmit.png" border=0> <?=VCDLanguage::translate('addmovie.imdb')?>
 <form action="./index.php?page=private&o=add&source=webfetch" method="post" name="imdb" id="imdb">
-<table cellspacing="1" cellpadding="1" class="plain">
-<tr>
-	<td><?=VCDLanguage::translate('addmovie.imdbtitle')?>:</td>
-	<td><input type="text" name="searchTitle" class="input" size="15"/></td><td rowspan="2">&nbsp; <?= display_fetchsites();?>&nbsp;<input type="submit" value="<?=VCDLanguage::translate('search.search')?>" class="buttontext"/></td>
-</tr>
-<tr>
-	<td><?=VCDLanguage::translate('addmovie.id')?>:</td><td><input type="text" name="fid" class="input" size="15"/></td>
-</tr>
-</table>
+<p class="inner">
+	<?=VCDLanguage::translate('addmovie.imdbtitle')?>:
+	<input type="text" name="searchTitle" class="input" size="15"/>&nbsp; 
+	<?= display_fetchsites();?>&nbsp;
+	<input type="checkbox" value="1" name="searchIsId" class="nof" title="<?=VCDLanguage::translate('addmovie.id')?>"/>&nbsp;
+	<input type="submit" value="<?=VCDLanguage::translate('search.search')?>" class="buttontext"/>
+</p>
 </form>
-</ul>
+</p>
 
-<h2><a href="./?page=private&amp;o=add_manually">B) <?=VCDLanguage::translate('addmovie.manual')?></a></h2>
+<p class="outer"><img src="images/icons/film_edit.png" border=0> <a href="./?page=private&amp;o=add_manually"><?=VCDLanguage::translate('addmovie.manual')?></a></p>
 
-<h2><a href="./?page=private&amp;o=add_listed">C) <?=VCDLanguage::translate('addmovie.listed')?></a></h2>
+<p class="outer"><img src="images/icons/film_go.png" border=0> <a href="./?page=private&amp;o=add_listed"><?=VCDLanguage::translate('addmovie.listed')?></a></p>
 
-<h2>D) <?=VCDLanguage::translate('addmovie.xml')?></h2>
-<ul>
+
+<p class="outer"><img src="images/icons/feed.png" border=0> <?=VCDLanguage::translate('addmovie.xml')?>
 <form action="./index.php?page=private&o=add&source=xml" method="post" name="xml" enctype="multipart/form-data">
 <input type="hidden" value="xml" name="xml"/>
-<input type="hidden" name="MAX_FILE_SIZE" value="10000000" />
-<table cellspacing="1" cellpadding="1" class="plain">
-<tr>
-	<td nowrap><?=VCDLanguage::translate('addmovie.xmlfile')?>:</td>
-	<td><input type="file" name="xmlfile" class="input"/>&nbsp;<input type="submit" value="<?=VCDLanguage::translate('menu.submit')?>" class="input" onclick="return checkupload(this.form.xmlfile.value)"/>
-	<a title="Max upload filesize can be adjusted in the php.ini">(<?=VCDLanguage::translate('addmovie.maxfilesize')?>: <?= ini_get('upload_max_filesize')?>)</a>
-	</td>
-</tr>
-<tr>
-	<td colspan="2" valign="top">
-	<?=VCDLanguage::translate('addmovie.xmlnote')?>
+<p class="inner">
+<?=VCDLanguage::translate('addmovie.xmlfile')?>: <input type="file" name="xmlfile" class="input"/>&nbsp;
+<input type="submit" value="<?=VCDLanguage::translate('menu.submit')?>" class="input" onclick="return checkupload(this.form.xmlfile.value)"/>
+<a title="Max upload filesize can be adjusted in the php.ini">(<?=VCDLanguage::translate('addmovie.maxfilesize')?>: <?= ini_get('upload_max_filesize')?>)</a>
+<br/><br/>
+<?=VCDLanguage::translate('addmovie.xmlnote')?>
+</p></form>
 
-	</td>
-</tr>
-</table>
-</form>
-</ul>
+</p>
+</fieldset>
