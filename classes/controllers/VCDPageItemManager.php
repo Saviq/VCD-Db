759e6fc2aafbd6c2449f92717035b4bd61bd7dd3
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2007-10-18 22:55:11 +0000

    Stub the the manager controller.

---
+++ classes/controllers/VCDPageItemManager.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * VCD-db - a web based VCD/DVD Catalog system
+ * Copyright (C) 2003-2007 Konni - konni.com
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * @author  Hákon Birgisson <konni@konni.com>
+ * @package Kernel
+ * @version $Id: VCDPageItemManager.php 1066 2007-08-15 17:05:56Z konni $
+ * @since 0.90
+ */
+?>
+<?php
+require_once(dirname(__FILE__).'/VCDPageBaseItem.php');
+
+class VCDPageItemManager extends VCDPageBaseItem  {
+		
+	public function __construct(_VCDPageNode $node) {
+
+		parent::__construct($node);
+		
+		
+	}
+	
+	
+}
+?>
