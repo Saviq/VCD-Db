96575507325b85627d825c0e41da17290ba763da
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2007-11-12 01:23:27 +0000

    made obsolete by smarty.

--- modules/footer.php
+++
@@ -1,6 +0,0 @@
-<div id="ftr" align="center">
-<?
-printf(VCDLanguage::translate('misc.footer'), VCDUtils::getPageLoadTime(), VCDConnection::getQueryCount())
-?>
- <a href="http://vcddb.konni.com" target="_new" title="<?=VCDLanguage::translate('misc.footer_link')?>">VCD-db <?=VCDDB_VERSION?></a>
-</div>
--- modules/header.php
+++
@@ -1,5 +0,0 @@
-<div id="toplogo" onclick="location.href='./?'" title="Home"></div>
-<div id="hdr" align="center"></div>
-<div id="bar">
-<? display_topmenu() ?>  <?=VCDClassFactory::loadClass('VCDLanguage')->printDropdownBox()?>
-</div>
