23c678c20d385af80788ecbafd35cebe0fe3ffb3
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2007-11-14 23:58:12 +0000

    Implemented the collection overview popup.

---
+++ classes/controllers/VCDPageUserCollectionOverview.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * VCD-db - a web based VCD/DVD Catalog system
+ * Copyright (C) 2003-2007 Konni - konni.com
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * @author  Hákon Birgisson <konni@konni.com>
+ * @package Kernel
+ * @subpackage Controller
+ * @version $Id: VCDPageUserCollectionOverview.php 1066 2007-08-15 17:05:56Z konni $
+ * @since 0.90
+ */
+?>
+<?php
+class VCDPageUserCollectionOverview extends VCDBasePage {
+
+	public function __construct(_VCDPageNode $node) {
+		
+		parent::__construct($node);
+		/*
+		if (!$this->initPage()) {
+			VCDException::display('You have not added any movies yet<break>Try again after you have inserted some movies');
+		}
+		*/
+		$this->initPage();
+				
+	}
+	
+	
+	private function initPage() {
+		
+		
+		$userCategories = SettingsServices::getCategoriesInUseByUserID(VCDUtils::getUserID());
+        if (sizeof($userCategories) == 0) {
+           return false;
+        }
+        
+        $mediaTypes = SettingsServices::getMediaTypesInUseByUserID(VCDUtils::getUserID());
+		$arrMediaTypes = array();
+        
+        // Assign the mediatypes
+        $results = array();
+        foreach ($mediaTypes as $list) {
+        	$results[] = $list[1];
+        	array_push($arrMediaTypes, $list[0]);
+        }
+        $this->assign('statsMediatypes',$results);
+
+        
+        
+        $arrMediaTypes = array_flip($arrMediaTypes);
+
+        $results = array();
+        foreach ($userCategories as $categoryObj) {
+
+        	// Get data for this category from the services
+        	$mediaData = SettingsServices::getMediaCountByCategoryAndUserID(VCDUtils::getUserID(),$categoryObj->getID());
+        	// Create calculated data array
+        	$arrResults = $this->getCategoryResults($arrMediaTypes,  $mediaData);
+            // Push the total sum as the last entry
+            $arrResults[] = array_sum($arrResults);
+            // Pust to the results
+            $results[$categoryObj->getName(true)] = $arrResults;
+        }
+        
+        // Assign to the template
+        $this->assign('statsCategories',$results);
+
+        /*
+        print "<pre>";
+        print_r($results);
+        print "</pre>";
+        
+        */
+        
+        $results = array();
+        $totalSum = 0;
+        foreach ($mediaTypes as $mediaCount) {
+            $totalSum += $mediaCount[2];
+            $results[] = $mediaCount[2];
+        }
+        $results[] = $totalSum;
+		$this->assign('statsSums',$results);        
+		
+	}
+	
+	
+	
+	
+	/**
+	 * Get count of movies in each category
+	 *
+	 * @param array $catArr | Array of categories
+	 * @param array $dataArray | array of movies
+	 * @return array
+	 */
+	private function getCategoryResults($catArr, $dataArray) {
+		$resultArr = $catArr;
+		$keys = array_keys($resultArr);
+		foreach ($keys as $key) {
+			$resultArr[$key] = 0;
+		}
+		foreach ($dataArray as $inArr) {
+			$resultArr[$inArr[0]] = $inArr[1];
+		}
+		return $resultArr;
+	}
+		
+}
+
+?>
---
+++ pages/window.user.overview.tpl
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>VCD-db</title>
+	<meta http-equiv="Content-Type" content="text/html; charset={$pageCharset}"/>
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" type="text/css" href="{$pageStyle}" media="screen, projection"/>
+	<link rel="stylesheet" type="text/css" href="includes/css/global.css" media="screen, projection"/>
+	{$pageScripts}
+</head>
+<body onload="window.focus()" class="nobg">
+
+<div id="tabledata">
+<table cellspacing="1" cellpadding="1" border="0" width="100%">
+<tr>
+	<td>&nbsp;</td>
+	{foreach from=$statsMediatypes item=title name=mediatypes}
+	<td>{$title}</td>
+	{/foreach}
+	<td>&nbsp;</td>
+</tr>
+{foreach from=$statsCategories item=i key=categoryName name=mediatypes}
+<tr class="{cycle values="stata,statb"}">
+	<td>{$categoryName}</td>
+	{foreach from=$i item=j name=items}
+	<td align="right">{$j}</td>
+	{/foreach}
+</tr>	
+{/foreach}
+<tr class="{cycle values="stata,statb"}">
+	<td>&nbsp;</td>
+	{foreach from=$statsSums item=sums}
+	<td class="bold" align="right">{$sums}</td>
+	{/foreach}
+</tr>
+</table>
+</div>
+<hr/>
+<p align="center"><input onclick="window.close()" type="button" value="Loka"/></p>
+
+{literal}
+<script type="text/javascript">
+try {
+	var tHeight = document.getElementById("tabledata").offsetHeight + 90;
+	var tWidth = document.layers ? window.outerWidth : document.body.clientWidth;
+	window.resizeTo(tWidth,tHeight);
+} catch (ex) {}
+</script>
+{/literal}
+</body>
+</html>
