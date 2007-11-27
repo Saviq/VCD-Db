6d97eb2b397b726954af68c64bce8bd9f80df865
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2007-11-27 23:31:32 +0000

    "Got rid of unused functions.  Added documentation where missing. Moved function that were only called once to their caller class."

--- functions/WebFunctions.php
+++ functions/WebFunctions.php
@@ -15,45 +15,12 @@
  */
 ?>
 <?php
-
 /**
- * Draw the IMDB links for the movie page
+ * Redirect the browser to a specific url.
  *
- * @param string $imdb_id | The IMDB ID
+ * @param string $relative_url | The interlan url to redirect to
  */
-function display_imdbLinks($imdb_id) {
-
-	print "<h2>".VCDLanguage::translate('imdb.links')."</h2>";
-	print "<ul>";
-	print "<li><a href=\"http://www.imdb.com/Title?".$imdb_id."\" target=\"new\">".VCDLanguage::translate('imdb.details')."</a></li>";
-	print "<li><a href=\"http://www.imdb.com/Plot?".$imdb_id."\" target=\"new\">".VCDLanguage::translate('imdb.plot')."</a></li>";
-	print "<li><a href=\"http://www.imdb.com/Gallery?".$imdb_id."\" target=\"new\">".VCDLanguage::translate('imdb.gallery')."</a></li>";
-	print "<li><a href=\"http://www.imdb.com/Trailers?".$imdb_id."\" target=\"new\">".VCDLanguage::translate('imdb.trailers')."</a></li>";
-	print "</ul>";
-}
-
-/**
- * Enter description here...
- *
- * @return unknown
- */
-function server_url()
-{
-   $proto = "http" . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "s" : "") . "://";
-   $server = isset($_SERVER['HTTP_HOST']) ?  $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
-
-   return $proto . $server;
-}
-
-
-// Default redirection to home
-/**
- * Enter description here...
- *
- * @param unknown $relative_url
- */
-function redirect($relative_url = '?')
-{
+function redirect($relative_url = '?') {
    
 	$url = str_replace(basename($_SERVER['PHP_SELF']),'',
 	    sprintf('http%s://%s%s',(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 's': ''),
@@ -61,8 +28,7 @@ function redirect($relative_url = '?')
 	
    $url .= $relative_url;
       
-   if (!headers_sent())
-   {
+   if (!headers_sent()) {
        header("Location: $url");
        exit();
    } else {
@@ -73,59 +39,35 @@ function redirect($relative_url = '?')
 
 
 /**
- * Enter description here...
+ * Sort an array by the a specific index
  *
- * @param unknown $multiArray
- * @param unknown $secondIndex
- * @return unknown
+ * @param array $multiArray | The array to sort
+ * @param mixed $secondIndex | The sort index
+ * @return array | The sorted array
  */
 function aSortBySecondIndex($multiArray, $secondIndex) {
-		   while (list($firstIndex, ) = each($multiArray))
-		       $indexMap[$firstIndex] = $multiArray[$firstIndex][$secondIndex];
-		   asort($indexMap);
-		   while (list($firstIndex, ) = each($indexMap))
-		       if (is_numeric($firstIndex))
-		           $sortedArray[] = $multiArray[$firstIndex];
-		       else $sortedArray[$firstIndex] = $multiArray[$firstIndex];
-		   return $sortedArray;
-}
-
-
-
-/**
- * Enter description here...
- *
- * @param unknown $str
- * @return unknown
- */
-function fixFormat($str) {
-	$len = 10;
-	$cats = explode(",",$str);
-	asort($cats);
-	if (sizeof($cats) == 1) {
-		return $cats[0];
-	}
-	$catstr = $str;
-	if (strlen($catstr) > $len) {
-		return "<span title=\"".$str."\">".$cats[0].", ...</span>";
-	} else {
-		return implode(",", $cats);
-	}
+	while (list($firstIndex, ) = each($multiArray))
+	   $indexMap[$firstIndex] = $multiArray[$firstIndex][$secondIndex];
+	asort($indexMap);
+	while (list($firstIndex, ) = each($indexMap))
+	   if (is_numeric($firstIndex))
+	       $sortedArray[] = $multiArray[$firstIndex];
+	   else $sortedArray[$firstIndex] = $multiArray[$firstIndex];
+	return $sortedArray;
 }
 
 
 /**
- * Enter description here...
+ * Get the category list localized and sorted
  *
- * @param unknown $categoryObjArr
- * @return unknown
+ * @param array $categoryObjArr | Array of moviecategoryObj
+ * @return array | The localized and sorted array
  */
 function getLocalizedCategories($categoryObjArr = null) {
 	
 	if ($categoryObjArr == null) {
 		$categoryObjArr = SettingsServices::getAllMovieCategories();
 	}
-
 
 	// Translate category names
 	$mapping = VCDUtils::getCategoryMapping();
@@ -140,37 +82,42 @@ function getLocalizedCategories($categoryObjArr = null) {
 }
 
 
-
-function human_file_size($size)
-{
+/**
+ * Get filesize in human readable form
+ *
+ * @param float $size
+ * @return string | The formatted filesize string
+ */
+function human_file_size($size) {
    if (is_numeric($size) && $size > 0) {
    		$filesizename = array(" Bytes", " kb", " MB", " GB", " TB", " PB", " EB", " ZB", " YB");
    		return round($size/pow(1024, ($i = floor(log($size, 1024)))), 2) . $filesizename[$i];
    } else {
-   		return "0 Bytes";
+   		return '0 Bytes';
    }
 }
 
  
+/**
+ * Translate a category name to the currently used language.
+ *
+ * @param string $category_name | The category name in english
+ * @return string | The category name in the currently used language
+ */
 function getLocalizedCategoryName($category_name) {
-
 	$baseMap = getCategoryMapping();
 	if (key_exists($category_name, $baseMap)) {
-
 		$baseKey = $baseMap[$category_name];
 		$translatedKey = VCDLanguage::translate($baseKey);
-		$notfound = "undefined";
+		$notfound = 'undefined';
 		if (strcmp($translatedKey, strtolower($notfound)) == 0) {
 			return $category_name;
 		} else {
 			return $translatedKey;
 		}
-
 	} else {
 		return $category_name;
 	}
 }
-
-
 
 ?>
