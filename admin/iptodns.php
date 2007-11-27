2378108e6eb670e7e7621d3bfb2c16e2ce2ab13f
konni <konni@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2007-11-27 22:56:25 +0000

    "Fixed code using php short tags."

--- admin/iptodns.php
+++ admin/iptodns.php
@@ -5,7 +5,7 @@
 	<style>
     body {
  	background: #F6F6F6;
-	font: 11px/1.2 Verdana, Arial, Helvetica, sans-serif; /*background:#ddd;*/
+	font: 11px/1.2 Verdana, Arial, Helvetica, sans-serif;
 	margin: 1px;
 	padding: 1px;
 }
@@ -13,7 +13,7 @@
 
 </head>
 <body leftmargin="0" topmargin="0">
-<?
+<?php
 echo gethostbyaddr($_GET['ip']);
 ?>
 </body>
