fd73918ecea8f0441af70f9857b7599b46b71398
saviq <saviq@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2007-12-02 20:18:55 +0000

    accidental commit...

--- classes/controllers/VCDPageItemMovie.php
+++ classes/controllers/VCDPageItemMovie.php
@@ -38,7 +38,6 @@ class VCDPageItemMovie extends VCDPageBaseItem  {
 	private function doCast() {
 		if (!is_null($this->sourceObj))	{
 			$this->assign('sourceActors', $this->sourceObj->getCast(true));
-			die(print_r($this->sourceObj->getCast(true), true));
 		}
 	}
 	
