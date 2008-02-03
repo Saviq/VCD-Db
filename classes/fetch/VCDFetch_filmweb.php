b0661ccd3ab51977ee749917f2fe51d11a6a8311
saviq <saviq@71fff97c-14aa-11df-8d95-b9b6dbe38d4c>
2008-02-03 18:33:59 +0000

    Fixes for new filmweb cast format

--- classes/fetch/VCDFetch_filmweb.php
+++ classes/fetch/VCDFetch_filmweb.php
@@ -27,7 +27,7 @@ class VCDFetch_filmweb extends VCDFetch {
 	'director' 	=> '#yseria(?:[^>]*>[^<]+</a>)+\s*scenariusz#',
 	'genre' 	=> 'genreIds[^>]*>([^<]*)</a>',
 	'rating' 	=> '#([0-9]{1,2}),([0-9]{1,2})<\/b>\/10#',
-	'cast'		=> 'class=\"filmActor\"[^>]+>([^<]+)</a>([^>]+>[^>]+>[^>]+>[^"]+"filmRole">([^<]+)<)?',
+	'cast'		=> 'class=\"nm\">[^<]+<a[^>]+>([^<]+)</a>([^>]+>[^>]+>[^>]+>[^>]+>([^<]+)<span)?',
 	'runtime' 	=> '#trwania: ([0-9]+)#i',
 	'country'	=> 'countryIds[^>]*>([^<]*)</a>',
 	'plot'		=> '#"justify">(.*?)</li>#'
